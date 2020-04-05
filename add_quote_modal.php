26e9f7a34b5fb397849b551f6c24e245a36a389d
johnny@pittpc.com <admin@app.pittpc.com>
2020-04-04 23:07:32 -0400

    Sort dropdowns clients, categories in add, edit quote alphabetically

--- add_quote_modal.php
+++ add_quote_modal.php
@@ -25,7 +25,7 @@
                 <option value="">- Client -</option>
                 <?php 
                 
-                $sql = mysqli_query($mysqli,"SELECT * FROM clients WHERE company_id = $session_company_id"); 
+                $sql = mysqli_query($mysqli,"SELECT * FROM clients WHERE company_id = $session_company_id ORDER BY client_name ASC"); 
                 while($row = mysqli_fetch_array($sql)){
                   $client_id = $row['client_id'];
                   $client_name = $row['client_name'];
@@ -60,7 +60,7 @@
                 <option value="">- Category -</option>
                 <?php 
                 
-                $sql = mysqli_query($mysqli,"SELECT * FROM categories WHERE category_type = 'Income' AND company_id = $session_company_id"); 
+                $sql = mysqli_query($mysqli,"SELECT * FROM categories WHERE category_type = 'Income' AND company_id = $session_company_id ORDER BY category_name ASC"); 
                 while($row = mysqli_fetch_array($sql)){
                   $category_id = $row['category_id'];
                   $category_name = $row['category_name'];
--- edit_quote_modal.php
+++ edit_quote_modal.php
@@ -32,7 +32,7 @@
                 <option value="">- Category -</option>
                 <?php 
                 
-                $sql_income_category = mysqli_query($mysqli,"SELECT * FROM categories WHERE category_type = 'Income' AND company_id = $session_company_id"); 
+                $sql_income_category = mysqli_query($mysqli,"SELECT * FROM categories WHERE category_type = 'Income' AND company_id = $session_company_id ORDER BY category_name ASC"); 
                 while($row = mysqli_fetch_array($sql_income_category)){
                   $category_id_select = $row['category_id'];
                   $category_name_select = $row['category_name'];
