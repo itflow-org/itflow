a2bd898eec297f12cbabcc79baf029b0792cbaac
johnny@pittpc.com <admin@app.pittpc.com>
2020-09-25 16:07:44 -0400

    Fixed Revenues Table List displaying Table Header fields when no fields exist

--- revenues.php
+++ revenues.php
@@ -69,7 +69,7 @@
     <hr>
     <div class="table-responsive">
       <table class="table table-striped table-borderless table-hover">
-        <thead class="text-dark">
+        <thead class="<?php if($num_rows[0] == 0){ echo "d-none"; } ?>">
           <tr>
             <th><a class="text-dark" href="?<?php echo $url_query_strings_sb; ?>&sb=revenue_date&o=<?php echo $disp; ?>">Date</a></th>
             <th><a class="text-dark" href="?<?php echo $url_query_strings_sb; ?>&sb=category_name&o=<?php echo $disp; ?>">Category</a></th>
