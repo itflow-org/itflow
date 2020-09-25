6e5a65ecb168459aef4550db85fba763016590c7
johnny@pittpc.com <admin@app.pittpc.com>
2020-09-25 17:16:02 -0400

    Added PHP Truncate Function to functions.php

--- functions.php
+++ functions.php
@@ -172,4 +172,15 @@ function get_device(){
     }   
 }
 
+function truncate($text, $chars = 25) {
+    if (strlen($text) <= $chars) {
+        return $text;
+    }
+    $text = $text." ";
+    $text = substr($text,0,$chars);
+    $text = substr($text,0,strrpos($text,' '));
+    $text = $text."...";
+    return $text;
+}
+
 ?>
