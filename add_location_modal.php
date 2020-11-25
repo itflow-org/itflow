ec474cb80e11861630c6c15e84e28e88addacc2c
Johnny <johnny@pop-os.localdomain>
2020-11-24 23:16:39 -0500

    Moved Country in edit/add location to bottom

--- add_location_modal.php
+++ add_location_modal.php
@@ -60,21 +60,6 @@
             <div class="tab-pane fade" id="pills-address">
 
               <div class="form-group">
-                <label>Country</label>
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-fw fa-flag"></i></span>
-                  </div>
-                  <select class="form-control select2" name="country">
-                    <option value="">- Country -</option>
-                    <?php foreach($countries_array as $country_name) { ?>
-                    <option><?php echo $country_name; ?></option>
-                    <?php } ?>
-                  </select>
-                </div>
-              </div>
-
-              <div class="form-group">
                 <label>Address</label>
                 <div class="input-group">
                   <div class="input-group-prepend">
@@ -116,6 +101,21 @@
                     <span class="input-group-text"><i class="fab fa-fw fa-usps"></i></span>
                   </div>
                   <input type="text" class="form-control" name="zip" placeholder="Postal Code">
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label>Country</label>
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fa fa-fw fa-flag"></i></span>
+                  </div>
+                  <select class="form-control select2" name="country">
+                    <option value="">- Country -</option>
+                    <?php foreach($countries_array as $country_name) { ?>
+                    <option><?php echo $country_name; ?></option>
+                    <?php } ?>
+                  </select>
                 </div>
               </div>
 
--- edit_location_modal.php
+++ edit_location_modal.php
@@ -60,21 +60,6 @@
             </div>
 
             <div class="tab-pane fade" id="pills-address<?php echo $location_id; ?>">
-             
-              <div class="form-group">
-                <label>Country</label>
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-fw fa-flag"></i></span>
-                  </div>
-                  <select class="form-control select2" name="country">
-                    <option value="">- Country -</option>
-                    <?php foreach($countries_array as $country_name) { ?>
-                    <option <?php if($location_country == $country_name) { echo "selected"; } ?>><?php echo $country_name; ?></option>
-                    <?php } ?>
-                  </select>
-                </div>
-              </div>
 
               <div class="form-group">
                 <label>Address</label>
@@ -117,6 +102,21 @@
                     <span class="input-group-text"><i class="fab fa-fw fa-usps"></i></span>
                   </div>
                   <input type="text" class="form-control" name="zip" placeholder="Postal Code" value="<?php echo $location_zip; ?>">
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label>Country</label>
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fa fa-fw fa-flag"></i></span>
+                  </div>
+                  <select class="form-control select2" name="country">
+                    <option value="">- Country -</option>
+                    <?php foreach($countries_array as $country_name) { ?>
+                    <option <?php if($location_country == $country_name) { echo "selected"; } ?>><?php echo $country_name; ?></option>
+                    <?php } ?>
+                  </select>
                 </div>
               </div>
 
