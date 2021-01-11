07de7bf3f274512eaded4e5f09fc84c5a3b244b5
Nymall <pythagorus1987@gmail.com>
2021-01-11 11:14:43 -0800

    Update Taxes.php
    Remove canadian tax items

--- Taxes.php
+++ Taxes.php
@@ -1,4 +1,3 @@
 <option <?php if($item_tax == '0.00'){ echo "selected"; } ?> value="0.00">None</option>
 <option <?php if($item_tax == '0.07'){ echo "selected"; } ?> value="0.07">State Tax 7%</option>
-<option <?php if($item_tax == '0.05'){ echo "selected"; } ?> value="0.05">Sales Tax 5%</option>
-<option <?php if($item_tax == '0.12'){ echo "selected"; } ?> value="0.12">BC Sales Tax 12%</option>
+
