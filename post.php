60600c786181aa13a5b04bf21f08832651cab385
johnny@pittpc.com <admin@app.pittpc.com>
2021-01-15 20:38:38 -0500

    Show Client Phone extension and mobile if applicable on invoice/quotes/recurring

--- post.php
+++ post.php
@@ -1842,7 +1842,12 @@ if(isset($_GET['pdf_quote'])){
     $client_email = $row['client_email'];
     $client_phone = $row['client_phone'];
     if(strlen($client_phone)>2){ 
-    $client_phone = substr($row['client_phone'],0,3)."-".substr($row['client_phone'],3,3)."-".substr($row['client_phone'],6,4);
+      $client_phone = substr($row['client_phone'],0,3)."-".substr($row['client_phone'],3,3)."-".substr($row['client_phone'],6,4);
+    }
+    $client_extension = $row['client_extension'];
+    $client_mobile = $row['client_mobile'];
+    if(strlen($client_mobile)>2){ 
+      $client_mobile = substr($row['client_mobile'],0,3)."-".substr($row['client_mobile'],3,3)."-".substr($row['client_mobile'],6,4);
     }
     $client_website = $row['client_website'];
 
