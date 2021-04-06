9318f40fcda8f411e1d441c9ed2ec578be14ad6b
johnnyq <johnnyq@users.noreply.github.com>
2021-04-06 01:40:17 -0400

    Initial work on detion confirmation

--- clients.php
+++ clients.php
@@ -222,7 +222,7 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli,"SELECT FOUND_ROWS()"));
                 <div class="dropdown-menu">
                   <a class="dropdown-item" href="#" data-toggle="modal" data-target="#editClientModal<?php echo $client_id; ?>">Edit</a>
                   <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="post.php?delete_client=<?php echo $client_id; ?>">Delete</a>
+                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#deleteClientModal<?php echo $client_id; ?>">Delete</a>
                 </div>
               </div>
             </td>
@@ -231,6 +231,7 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli,"SELECT FOUND_ROWS()"));
           <?php
 
             include("edit_client_modal.php");
+            include("delete_client_modal.php");
 
           }
           
