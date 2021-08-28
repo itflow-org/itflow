f8cbe83c174a144a3f23a9dcb9ec1c0b28de02d0
johnnyq <pittpc@web01.pittpc.com>
2021-08-28 15:09:36 -0400

    Updated Add/Edit Transfer UI to include notes in one pane also removed experimental check deposit code in transfer it will not work with split payments between invoices therefore it wont work

--- add_transfer_modal.php
+++ add_transfer_modal.php
@@ -10,161 +10,111 @@
       <form action="post.php" method="post" autocomplete="off">
         <div class="modal-body bg-white">
 
-          <ul class="nav nav-pills nav-justified mb-3">
-            <li class="nav-item">
-              <a class="nav-link active" data-toggle="pill" href="#pills-details">Details</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-toggle="pill" href="#pills-assign">Assign</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-toggle="pill" href="#pills-notes">Notes</a>
-            </li>
-          </ul>
-
-          <hr>
-
-          <div class="tab-content">
-
-            <div class="tab-pane fade show active" id="pills-details">
-
-              <div class="form-row">
+          <div class="form-row">
+            
+            <div class="form-group col">
+              <label>Date <strong class="text-danger">*</strong></label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class="fa fa-fw fa-calendar"></i></span>
+                </div>
+                <input type="date" class="form-control" name="date" value="<?php echo date("Y-m-d"); ?>" required>
+              </div>
+            </div>
+            
+            <div class="form-group col">
+              <label>Amount <strong class="text-danger">*</strong></label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class="fa fa-fw fa-dollar-sign"></i></span>
+                </div>
+                <input type="number" class="form-control" step="0.01" min="0" name="amount" placeholder="Amount to transfer" required autofocus>
+              </div>
+            </div>
+          
+          </div>
+       
+          <div class="form-group">
+            <label>Transfer <strong class="text-danger">*</strong></label>
+            <div class="input-group"> 
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fa fa-fw fa-piggy-bank"></i></span>
+              </div> 
+              <select class="form-control select2" name="account_from" required>
+                <option value="">- Account From -</option>
+                <?php 
                 
-                <div class="form-group col">
-                  <label>Date <strong class="text-danger">*</strong></label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="fa fa-fw fa-calendar"></i></span>
-                    </div>
-                    <input type="date" class="form-control" name="date" value="<?php echo date("Y-m-d"); ?>" required>
-                  </div>
-                </div>
+                $sql = mysqli_query($mysqli,"SELECT * FROM accounts WHERE account_archived_at IS NULL AND company_id = $session_company_id ORDER BY account_name ASC"); 
+                while($row = mysqli_fetch_array($sql)){
+                  $account_id = $row['account_id'];
+                  $account_name = $row['account_name'];
+                  $opening_balance = $row['opening_balance'];
+                  
+                  $sql_payments = mysqli_query($mysqli,"SELECT SUM(payment_amount) AS total_payments FROM payments WHERE payment_account_id = $account_id");
+                  $row = mysqli_fetch_array($sql_payments);
+                  $total_payments = $row['total_payments'];
+                  
+                  $sql_revenues = mysqli_query($mysqli,"SELECT SUM(revenue_amount) AS total_revenues FROM revenues WHERE revenue_account_id = $account_id");
+                  $row = mysqli_fetch_array($sql_revenues);
+                  $total_revenues = $row['total_revenues'];
+
+                  $sql_expenses = mysqli_query($mysqli,"SELECT SUM(expense_amount) AS total_expenses FROM expenses WHERE expense_account_id = $account_id");
+                  $row = mysqli_fetch_array($sql_expenses);
+                  $total_expenses = $row['total_expenses'];
+
+                  $balance = $opening_balance + $total_payments + $total_revenues - $total_expenses;
                 
-                <div class="form-group col">
-                  <label>Amount <strong class="text-danger">*</strong></label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="fa fa-fw fa-dollar-sign"></i></span>
-                    </div>
-                    <input type="number" class="form-control" step="0.01" min="0" name="amount" placeholder="Amount to transfer" required autofocus>
-                  </div>
-                </div>
-              
-              </div>
-           
-              <div class="form-group">
-                <label>Transfer <strong class="text-danger">*</strong></label>
-                <div class="input-group"> 
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-fw fa-piggy-bank"></i></span>
-                  </div> 
-                  <select class="form-control select2" name="account_from" required>
-                    <option value="">- Account From -</option>
-                    <?php 
-                    
-                    $sql = mysqli_query($mysqli,"SELECT * FROM accounts WHERE account_archived_at IS NULL AND company_id = $session_company_id ORDER BY account_name ASC"); 
-                    while($row = mysqli_fetch_array($sql)){
-                      $account_id = $row['account_id'];
-                      $account_name = $row['account_name'];
-                      $opening_balance = $row['opening_balance'];
-                      
-                      $sql_payments = mysqli_query($mysqli,"SELECT SUM(payment_amount) AS total_payments FROM payments WHERE payment_account_id = $account_id");
-                      $row = mysqli_fetch_array($sql_payments);
-                      $total_payments = $row['total_payments'];
-                      
-                      $sql_revenues = mysqli_query($mysqli,"SELECT SUM(revenue_amount) AS total_revenues FROM revenues WHERE revenue_account_id = $account_id");
-                      $row = mysqli_fetch_array($sql_revenues);
-                      $total_revenues = $row['total_revenues'];
-
-                      $sql_expenses = mysqli_query($mysqli,"SELECT SUM(expense_amount) AS total_expenses FROM expenses WHERE expense_account_id = $account_id");
-                      $row = mysqli_fetch_array($sql_expenses);
-                      $total_expenses = $row['total_expenses'];
-
-                      $balance = $opening_balance + $total_payments + $total_revenues - $total_expenses;
-                    
-                    ?>
-                      <option <?php if($config_default_transfer_from_account == $account_id){ echo "selected"; } ?> value="<?php echo $account_id; ?>"><?php echo $account_name; ?> [$<?php echo number_format($balance,2); ?>]</option>
-                    
-                    <?php
-                    }
-                    ?>
-                  </select>
-                </div>
-              </div>
-
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-fw fa-arrow-right"></i></span>
-                  </div>
-                  <select class="form-control select2" name="account_to" required>
-                    <option value="">- Account To -</option>
-                    <?php 
-                    
-                    $sql = mysqli_query($mysqli,"SELECT * FROM accounts WHERE account_archived_at IS NULL AND company_id = $session_company_id ORDER BY account_name ASC"); 
-                    while($row = mysqli_fetch_array($sql)){
-                      $account_id = $row['account_id'];
-                      $account_name = $row['account_name'];
-                      $opening_balance = $row['opening_balance'];
-                    
-                      $sql_payments = mysqli_query($mysqli,"SELECT SUM(payment_amount) AS total_payments FROM payments WHERE payment_account_id = $account_id");
-                      $row = mysqli_fetch_array($sql_payments);
-                      $total_payments = $row['total_payments'];
-
-                      $sql_revenues = mysqli_query($mysqli,"SELECT SUM(revenue_amount) AS total_revenues FROM revenues WHERE revenue_account_id = $account_id");
-                      $row = mysqli_fetch_array($sql_revenues);
-                      $total_revenues = $row['total_revenues'];
-                      
-                      $sql_expenses = mysqli_query($mysqli,"SELECT SUM(expense_amount) AS total_expenses FROM expenses WHERE expense_account_id = $account_id");
-                      $row = mysqli_fetch_array($sql_expenses);
-                      $total_expenses = $row['total_expenses'];
-
-                      $balance = $opening_balance + $total_payments + $total_revenues - $total_expenses;
-
-                    ?>
-                      <option <?php if($config_default_transfer_to_account == $account_id){ echo "selected"; } ?> value="<?php echo $account_id; ?>"><?php echo $account_name; ?> [$<?php echo number_format($balance,2); ?>]</option>
-                    
-                    <?php
-                    }
-                    ?>
-                  </select>
-                </div>
-              </div>
+                ?>
+                  <option <?php if($config_default_transfer_from_account == $account_id){ echo "selected"; } ?> value="<?php echo $account_id; ?>"><?php echo $account_name; ?> [$<?php echo number_format($balance,2); ?>]</option>
+                
+                <?php
+                }
+                ?>
+              </select>
             </div>
+          </div>
 
-            <div class="tab-pane fade" id="pills-assign">
-
-              <?php
-
-              //UNFINISHED BUSINESS (The goal is to select checks that need deposited and autocalculate)
-
-              $sql_payments = mysqli_query($mysqli,"SELECT * FROM payments WHERE payment_account_id = 1");
-
-              while($row = mysqli_fetch_array($sql_payments)){
-                $payment_id = $row['payment_id'];
-                $payment_method = $row['payment_method'];
-                $payment_amount = $row['payment_amount'];
-              ?>
-
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" name="payments[]" value="<?php echo $payment_id; ?>">
-                  <label class="form-check-label"><?php echo "$payment_method - $payment_amount"; ?></label>
-                </div>
-
-              <?php
-              }
-              ?>
-
-            </div>
-
-            <div class="tab-pane fade" id="pills-notes">
-              
-              <div class="form-group">
-                <textarea class="form-control" rows="8" name="notes"></textarea>
+          <div class="form-group">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fa fa-fw fa-arrow-right"></i></span>
               </div>
+              <select class="form-control select2" name="account_to" required>
+                <option value="">- Account To -</option>
+                <?php 
+                
+                $sql = mysqli_query($mysqli,"SELECT * FROM accounts WHERE account_archived_at IS NULL AND company_id = $session_company_id ORDER BY account_name ASC"); 
+                while($row = mysqli_fetch_array($sql)){
+                  $account_id = $row['account_id'];
+                  $account_name = $row['account_name'];
+                  $opening_balance = $row['opening_balance'];
+                
+                  $sql_payments = mysqli_query($mysqli,"SELECT SUM(payment_amount) AS total_payments FROM payments WHERE payment_account_id = $account_id");
+                  $row = mysqli_fetch_array($sql_payments);
+                  $total_payments = $row['total_payments'];
 
+                  $sql_revenues = mysqli_query($mysqli,"SELECT SUM(revenue_amount) AS total_revenues FROM revenues WHERE revenue_account_id = $account_id");
+                  $row = mysqli_fetch_array($sql_revenues);
+                  $total_revenues = $row['total_revenues'];
+                  
+                  $sql_expenses = mysqli_query($mysqli,"SELECT SUM(expense_amount) AS total_expenses FROM expenses WHERE expense_account_id = $account_id");
+                  $row = mysqli_fetch_array($sql_expenses);
+                  $total_expenses = $row['total_expenses'];
+
+                  $balance = $opening_balance + $total_payments + $total_revenues - $total_expenses;
+
+                ?>
+                  <option <?php if($config_default_transfer_to_account == $account_id){ echo "selected"; } ?> value="<?php echo $account_id; ?>"><?php echo $account_name; ?> [$<?php echo number_format($balance,2); ?>]</option>
+                
+                <?php
+                }
+                ?>
+              </select>
             </div>
+          </div>
 
+          <div class="form-group">
+            <textarea class="form-control" rows="4" name="notes" placeholder="Enter some notes"></textarea>
           </div>
 
         </div>
--- edit_transfer_modal.php
+++ edit_transfer_modal.php
@@ -8,136 +8,117 @@
         </button>
       </div>
       <form action="post.php" method="post" autocomplete="off">
+        <input type="hidden" name="transfer_id" value="<?php echo $transfer_id; ?>">
+        <input type="hidden" name="expense_id" value="<?php echo $expense_id; ?>">
+        <input type="hidden" name="revenue_id" value="<?php echo $revenue_id; ?>">
+        
         <div class="modal-body bg-white">
-          <input type="hidden" name="transfer_id" value="<?php echo $transfer_id; ?>">
-          <input type="hidden" name="expense_id" value="<?php echo $expense_id; ?>">
-          <input type="hidden" name="revenue_id" value="<?php echo $revenue_id; ?>">
           
-          <ul class="nav nav-pills nav-justified mb-3">
-            <li class="nav-item">
-              <a class="nav-link active" data-toggle="pill" href="#pills-details<?php echo $transfer_id; ?>">Details</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-toggle="pill" href="#pills-notes<?php echo $transfer_id; ?>">Notes</a>
-            </li>
-          </ul>
-
-          <div class="tab-content">
-
-            <div class="tab-pane fade show active" id="pills-details<?php echo $transfer_id; ?>">
-
-              <div class="form-row">
-                
-                <div class="form-group col-sm">
-                  <label>Date <strong class="text-danger">*</strong></label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="fa fa-fw fa-calendar"></i></span>
-                    </div>
-                    <input type="date" class="form-control" name="date" value="<?php echo $transfer_date; ?>" required>
-                  </div>
+          <div class="form-row">
+            
+            <div class="form-group col-sm">
+              <label>Date <strong class="text-danger">*</strong></label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class="fa fa-fw fa-calendar"></i></span>
                 </div>
-                
-                <div class="form-group col-sm">
-                  <label>Amount <strong class="text-danger">*</strong></label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="fa fa-fw fa-dollar-sign"></i></span>
-                    </div>
-                    <input type="number" class="form-control" step="0.01" min="0" name="amount" placeholder="Amount to transfer" value="<?php echo $transfer_amount; ?>" required>
-                  </div>
-                </div>
-              
+                <input type="date" class="form-control" name="date" value="<?php echo $transfer_date; ?>" required>
               </div>
-              
-              <div class="form-group">
-                <label>Transfer <strong class="text-danger">*</strong></label>
-                <div class="input-group"> 
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-fw fa-piggy-bank"></i></span>
-                  </div> 
-                  <select class="form-control select2" name="account_from" required>
-                    <?php 
-                    
-                    $sql_accounts = mysqli_query($mysqli,"SELECT * FROM accounts WHERE (account_archived_at > '$transfer_created_at' OR account_archived_at IS NULL) AND company_id = $session_company_id ORDER BY account_name ASC"); 
-                      while($row = mysqli_fetch_array($sql_accounts)){
-                        $account_id_select = $row['account_id'];
-                        $account_name_select = $row['account_name'];
-                        $opening_balance = $row['opening_balance'];
-                        
-                        $sql_payments = mysqli_query($mysqli,"SELECT SUM(payment_amount) AS total_payments FROM payments WHERE payment_account_id = $account_id_select");
-                        $row = mysqli_fetch_array($sql_payments);
-                        $total_payments = $row['total_payments'];
-                        
-                        $sql_revenues = mysqli_query($mysqli,"SELECT SUM(revenue_amount) AS total_revenues FROM revenues WHERE revenue_account_id = $account_id_select");
-                        $row = mysqli_fetch_array($sql_revenues);
-                        $total_revenues = $row['total_revenues'];
-
-                        $sql_expenses = mysqli_query($mysqli,"SELECT SUM(expense_amount) AS total_expenses FROM expenses WHERE expense_account_id = $account_id_select");
-                        $row = mysqli_fetch_array($sql_expenses);
-                        $total_expenses = $row['total_expenses'];
-
-                        $balance = $opening_balance + $total_payments + $total_revenues - $total_expenses;
-                    
-                    ?>
-                    <option <?php if($transfer_account_from == $account_id_select){ echo "selected"; } ?> value="<?php echo $account_id_select; ?>"><?php echo $account_name_select; ?> [$<?php echo number_format($balance,2); ?>]</option>
-                    <?php
-                    }
-                    
-                    ?>
-                  </select>
-                </div>
-              </div>
-              
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-fw fa-arrow-right"></i></span>
-                  </div>
-                  <select class="form-control select2" name="account_to" required>
-                    <?php 
-                    
-                    $sql2 = mysqli_query($mysqli,"SELECT * FROM accounts WHERE (account_archived_at > '$transfer_created_at' OR account_archived_at IS NULL) AND company_id = $session_company_id ORDER BY account_name ASC"); 
-                    while($row = mysqli_fetch_array($sql2)){
-                      $account_id2 = $row['account_id'];
-                      $account_name = $row['account_name'];
-                      $opening_balance = $row['opening_balance'];
-
-                      $sql_payments = mysqli_query($mysqli,"SELECT SUM(payment_amount) AS total_payments FROM payments WHERE payment_account_id = $account_id2");
-                      $row = mysqli_fetch_array($sql_payments);
-                      $total_payments = $row['total_payments'];
-
-                      $sql_revenues = mysqli_query($mysqli,"SELECT SUM(revenue_amount) AS total_revenues FROM revenues WHERE revenue_account_id = $account_id2");
-                      $row = mysqli_fetch_array($sql_revenues);
-                      $total_revenues = $row['total_revenues'];
-                      
-                      $sql_expenses = mysqli_query($mysqli,"SELECT SUM(expense_amount) AS total_expenses FROM expenses WHERE expense_account_id = $account_id2");
-                      $row = mysqli_fetch_array($sql_expenses);
-                      $total_expenses = $row['total_expenses'];
-
-                      $balance = $opening_balance + $total_payments + $total_revenues - $total_expenses;
-
-                    ?>
-                    <option <?php if($transfer_account_to == $account_id2){ echo "selected"; } ?> value="<?php echo $account_id2; ?>"><?php echo $account_name; ?> [$<?php echo number_format($balance,2); ?>]</option>
-                    <?php
-                    }
-                    
-                    ?>
-                  </select>
-                </div>
-              </div>
-
             </div>
-
-            <div class="tab-pane fade" id="pills-notes<?php echo $transfer_id; ?>">
-              
-              <div class="form-group">
-                <textarea class="form-control" rows="8" name="notes"><?php echo $transfer_notes; ?></textarea>
+            
+            <div class="form-group col-sm">
+              <label>Amount <strong class="text-danger">*</strong></label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class="fa fa-fw fa-dollar-sign"></i></span>
+                </div>
+                <input type="number" class="form-control" step="0.01" min="0" name="amount" placeholder="Amount to transfer" value="<?php echo $transfer_amount; ?>" required>
               </div>
-
             </div>
-
+          
           </div>
+          
+          <div class="form-group">
+            <label>Transfer <strong class="text-danger">*</strong></label>
+            <div class="input-group"> 
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fa fa-fw fa-piggy-bank"></i></span>
+              </div> 
+              <select class="form-control select2" name="account_from" required>
+                <?php 
+                
+                $sql_accounts = mysqli_query($mysqli,"SELECT * FROM accounts WHERE (account_archived_at > '$transfer_created_at' OR account_archived_at IS NULL) AND company_id = $session_company_id ORDER BY account_name ASC"); 
+                  while($row = mysqli_fetch_array($sql_accounts)){
+                    $account_id_select = $row['account_id'];
+                    $account_name_select = $row['account_name'];
+                    $opening_balance = $row['opening_balance'];
+                    
+                    $sql_payments = mysqli_query($mysqli,"SELECT SUM(payment_amount) AS total_payments FROM payments WHERE payment_account_id = $account_id_select");
+                    $row = mysqli_fetch_array($sql_payments);
+                    $total_payments = $row['total_payments'];
+                    
+                    $sql_revenues = mysqli_query($mysqli,"SELECT SUM(revenue_amount) AS total_revenues FROM revenues WHERE revenue_account_id = $account_id_select");
+                    $row = mysqli_fetch_array($sql_revenues);
+                    $total_revenues = $row['total_revenues'];
+
+                    $sql_expenses = mysqli_query($mysqli,"SELECT SUM(expense_amount) AS total_expenses FROM expenses WHERE expense_account_id = $account_id_select");
+                    $row = mysqli_fetch_array($sql_expenses);
+                    $total_expenses = $row['total_expenses'];
+
+                    $balance = $opening_balance + $total_payments + $total_revenues - $total_expenses;
+                
+                ?>
+                <option <?php if($transfer_account_from == $account_id_select){ echo "selected"; } ?> value="<?php echo $account_id_select; ?>"><?php echo $account_name_select; ?> [$<?php echo number_format($balance,2); ?>]</option>
+                <?php
+                }
+                
+                ?>
+              </select>
+            </div>
+          </div>
+          
+          <div class="form-group">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fa fa-fw fa-arrow-right"></i></span>
+              </div>
+              <select class="form-control select2" name="account_to" required>
+                <?php 
+                
+                $sql2 = mysqli_query($mysqli,"SELECT * FROM accounts WHERE (account_archived_at > '$transfer_created_at' OR account_archived_at IS NULL) AND company_id = $session_company_id ORDER BY account_name ASC"); 
+                while($row = mysqli_fetch_array($sql2)){
+                  $account_id2 = $row['account_id'];
+                  $account_name = $row['account_name'];
+                  $opening_balance = $row['opening_balance'];
+
+                  $sql_payments = mysqli_query($mysqli,"SELECT SUM(payment_amount) AS total_payments FROM payments WHERE payment_account_id = $account_id2");
+                  $row = mysqli_fetch_array($sql_payments);
+                  $total_payments = $row['total_payments'];
+
+                  $sql_revenues = mysqli_query($mysqli,"SELECT SUM(revenue_amount) AS total_revenues FROM revenues WHERE revenue_account_id = $account_id2");
+                  $row = mysqli_fetch_array($sql_revenues);
+                  $total_revenues = $row['total_revenues'];
+                  
+                  $sql_expenses = mysqli_query($mysqli,"SELECT SUM(expense_amount) AS total_expenses FROM expenses WHERE expense_account_id = $account_id2");
+                  $row = mysqli_fetch_array($sql_expenses);
+                  $total_expenses = $row['total_expenses'];
+
+                  $balance = $opening_balance + $total_payments + $total_revenues - $total_expenses;
+
+                ?>
+                <option <?php if($transfer_account_to == $account_id2){ echo "selected"; } ?> value="<?php echo $account_id2; ?>"><?php echo $account_name; ?> [$<?php echo number_format($balance,2); ?>]</option>
+                <?php
+                }
+                
+                ?>
+              </select>
+            </div>
+          </div>
+            
+          <div class="form-group">
+            <textarea class="form-control" rows="4" name="notes" placeholder="Enter some notes"><?php echo $transfer_notes; ?></textarea>
+          </div>
+
 
         </div>
 
