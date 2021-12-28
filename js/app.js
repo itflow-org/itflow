15d95a3647ce63d4b9436e25741718f91846a016
johnnyq <johnny@pittpc.com>
2021-12-28 18:20:15 -0500

    Set Feedback Alerts to disappear after 4 seconds instead of 2 seconds

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ if(window.history.replaceState){
   window.history.replaceState(null, null, window.location.href);
 }
 
-// Slide alert up after 2 secs
-$("#alert").fadeTo(2000, 500).slideUp(500, function(){
+// Slide alert up after 4 secs
+$("#alert").fadeTo(4000, 500).slideUp(500, function(){
   $("#alert").slideUp(500);
 });
 
