1dd55dbfe2d4b057e3e9f0d522f4a93416ed5aa1
Marcus Hill <bcf8c9f2@opayq.com>
2022-01-11 11:44:10 +0000

    Add config_api_key column back for now, to avoid confusion

--- db.sql
+++ db.sql
@@ -889,6 +889,7 @@ DROP TABLE IF EXISTS `settings`;
 CREATE TABLE `settings` (
   `company_id` int(11) NOT NULL,
   `config_api_key` varchar(200) DEFAULT NULL,
+  `config_aes_key` varchar(250) DEFAULT NULL COMMENT 'Legacy',
   `config_base_url` varchar(200) DEFAULT NULL,
   `config_smtp_host` varchar(200) DEFAULT NULL,
   `config_smtp_port` int(5) DEFAULT NULL,
