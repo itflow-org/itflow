c3a3ab8410efcf9dad135e3a179cf2f1a12f8282
Marcus Hill <bcf8c9f2@opayq.com>
2022-01-14 17:53:26 +0000

    Use global search for site search

--- top_nav.php
+++ top_nav.php
@@ -9,9 +9,9 @@
   </ul>
   
   <!-- SEARCH FORM -->
-  <form class="form-inline ml-auto mr-5" action="clients.php">
+  <form class="form-inline ml-auto mr-5" action="global_search.php">
     <div class="input-group input-group-sm">
-      <input class="form-control form-control-navbar" type="search" placeholder="Search" name="q">
+      <input class="form-control form-control-navbar" type="search" placeholder="Search" name="query">
       <div class="input-group-append">
         <button class="btn btn-navbar" type="submit">
           <i class="fas fa-search"></i>
