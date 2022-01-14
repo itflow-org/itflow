c5b11aebe5d2ec68a3b4e00a500cfa8d13d773b2
Marcus Hill <bcf8c9f2@opayq.com>
2022-01-14 17:54:05 +0000

    Tidy

--- header.php
+++ header.php
@@ -7,10 +7,6 @@
 ?>
 
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
 <head>
   <meta charset="utf-8">
