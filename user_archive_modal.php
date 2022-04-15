945288dfbd279193cae0922354b144e7138bcf3c
Marcus Hill <bcf8c9f2@opayq.com>
2022-04-15 17:54:23 +0100

    Replace center with css text align

--- user_archive_modal.php
+++ user_archive_modal.php
@@ -2,14 +2,14 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-body">
-        <center class="mb-4">
+        <div class="mb-4" style="text-align: center;">
           <i class="far fa-10x fa-times-circle text-danger mb-3 mt-3"></i>
           <h2>Are you sure?</h2>
           <h6 class="mb-4 text-secondary">Do you really want to <b>archive <?php echo $user_name; ?></b>? This process cannot be undone.</h6>
           <h6 class="mb-4 text-secondary"><?php echo $user_name ?> will no longer be able to login or use ITFlow, but all associated content will remain accessible.</h6>
           <button type="button" class="btn btn-outline-secondary btn-lg px-5 mr-4" data-dismiss="modal">Cancel</button>
           <a class="btn btn-danger btn-lg px-5" href="post.php?archive_user=<?php echo $user_id; ?>">Yes, archive!</a>
-        </center>
+        </div>
       </div>
     </div>
   </div>
