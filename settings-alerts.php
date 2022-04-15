9a48bfae498b0e0d55f9fde6167a372ab0c53e0e
johnnyq <johnny@pittpc.com>
2022-04-14 23:29:29 -0400

    Changed Cron wording to run time from 11PM to 1AM Daily

--- settings-alerts.php
+++ settings-alerts.php
@@ -9,7 +9,7 @@
 
       <div class="custom-control custom-switch mb-2">
         <input type="checkbox" class="custom-control-input" name="config_enable_cron" <?php if($config_enable_cron == 1){ echo "checked"; } ?> value="1" id="customSwitch1">
-        <label class="custom-control-label" for="customSwitch1">Enable Cron <small>(cron.php must also be added to cron and run nightly at 11:00PM for alerts to work)</small></label>
+        <label class="custom-control-label" for="customSwitch1">Enable Cron <small>(cron.php must also be added to cron and run daily at 1:00AM for alerts to work)</small></label>
       </div>
 
       <div class="custom-control custom-switch mb-2">
