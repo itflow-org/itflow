2d6e7dbb37dc6fffb21e4efbca5fa9b639ba898d
Marcus Hill <bcf8c9f2@opayq.com>
2022-04-18 15:59:09 +0100

    Add clients/read.php API endpoint. Adjust asset create/update so that they function without all attributes being provided. Update will default to using the value currently in the database, rather than overwriting blank.

---
+++ api/v1/clients/read.php
@@ -0,0 +1,24 @@
+<?php
+require('../validate_api_key.php');
+
+require('../require_get_method.php');
+
+// Specific client via ID (single)
+if(isset($_GET['client_id'])){
+  $id = intval($_GET['client_id']);
+  $sql = mysqli_query($mysqli, "SELECT * FROM clients WHERE client_id = '$id' AND client_id LIKE '$client_id' AND company_id = '$company_id'");
+}
+
+// Specific client via name (single)
+elseif(isset($_GET['client_name'])){
+  $name = trim(strip_tags(mysqli_real_escape_string($mysqli,$_GET['client_name'])));
+  $sql = mysqli_query($mysqli, "SELECT * FROM clients WHERE client_name = '$name' AND client_id LIKE '$client_id' AND company_id = '$company_id'");
+}
+
+// All clients
+else{
+  $sql = mysqli_query($mysqli, "SELECT * FROM clients WHERE client_id LIKE '$client_id' AND company_id = '$company_id' ORDER BY client_id LIMIT $limit OFFSET $offset");
+}
+
+// Output
+include("../read_output.php");
