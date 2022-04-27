00e0d1dfb37d982210c27c895b627abc779dfa05
johnnyq <johnny@pittpc.com>
2022-04-27 01:22:32 -0400

    Fix hidden documents update all documents to use document_folder_id 0 during DB Update

--- database_updates.php
+++ database_updates.php
@@ -165,8 +165,14 @@ if(LATEST_DATABASE_VERSION > CURRENT_DATABASE_VERSION){
   if(CURRENT_DATABASE_VERSION == '0.0.5'){
     // Insert queries here required to update to DB version 0.0.6
 
-    mysqli_query($mysqli, "DROP TABLE `documents_tagged`;");
-    mysqli_query($mysqli, "DROP TABLE `document_tags`;");
+    mysqli_query($mysqli, "UPDATE documents SET document_folder_id = 0");
+
+    mysqli_query($mysqli, "DROP TABLE documents_tagged");
+    mysqli_query($mysqli, "DROP TABLE document_tags");
+
+
+    // Then, update the database to the next sequential version
+    mysqli_query($mysqli, "UPDATE settings SET config_current_database_version = '0.0.6'");
   }
 
   if(CURRENT_DATABASE_VERSION == '0.0.6'){
