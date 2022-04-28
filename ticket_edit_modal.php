bc1426ba1823148169f21f48b54aa4888cf05571
Marcus Hill <bcf8c9f2@opayq.com>
2022-04-28 20:52:20 +0100

    Don't show archived users for ticket filters/assignment

--- ticket_edit_modal.php
+++ ticket_edit_modal.php
@@ -21,7 +21,7 @@
                 <option value="0">Not Assigned</option>
                 <?php 
                 
-                $sql_assign_to_select = mysqli_query($mysqli,"SELECT * FROM users, user_companies WHERE users.user_id = user_companies.user_id AND user_companies.company_id = $session_company_id ORDER BY user_name ASC");
+                $sql_assign_to_select = mysqli_query($mysqli,"SELECT * FROM users, user_companies WHERE users.user_id = user_companies.user_id AND user_companies.company_id = $session_company_id AND user_archived_at IS NULL ORDER BY user_name ASC");
                 while($row = mysqli_fetch_array($sql_assign_to_select)){
                   $user_id = $row['user_id'];
                   $user_name = $row['user_name'];
