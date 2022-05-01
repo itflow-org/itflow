c19328f3d8b1b8a35c3a1a897a5d52812f00c2ff
Marcus Hill <bcf8c9f2@opayq.com>
2022-05-01 10:37:07 +0100

    Typo

--- api/v1/delete_output.php
+++ api/v1/delete_output.php
@@ -8,12 +8,12 @@
 
 // Check if delete query was successful
 if(isset($delete_count) && is_numeric($delete_count) && $delete_count > 0){
-  // Insert successful
+  // Delete was successful
   $return_arr['success'] = "True";
   $return_arr['count'] = $delete_count;
 }
 
-// Query returned false: something went wrong, or it was declined due to required variables missing
+// Delete query returned false: something went wrong, or it was declined due to required variables missing
 else{
   $return_arr['success'] = "False";
   $return_arr['message'] = "Auth success but delete query failed. Ensure ALL required variables are provided and database schema is up-to-date. Most likely cause: asset/client/company ID mismatch.";
