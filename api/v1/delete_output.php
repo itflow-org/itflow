69c1fc6cae0d45c9c105977f8a781f8eacd4584d
Marcus Hill <bcf8c9f2@opayq.com>
2022-05-01 10:30:04 +0100

    Add asset API delete endpoint

---
+++ api/v1/delete_output.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * API - delete_output.php
+ * Included on calls to delete.php endpoints
+ * Returns success/failure messages
+ */
+
+// Check if delete query was successful
+if(isset($delete_count) && is_numeric($delete_count) && $delete_count > 0){
+  // Insert successful
+  $return_arr['success'] = "True";
+  $return_arr['count'] = $delete_count;
+}
+
+// Query returned false: something went wrong, or it was declined due to required variables missing
+else{
+  $return_arr['success'] = "False";
+  $return_arr['message'] = "Auth success but delete query failed. Ensure ALL required variables are provided and database schema is up-to-date. Most likely cause: asset/client/company ID mismatch.";
+}
+
+echo json_encode($return_arr);
+exit();
