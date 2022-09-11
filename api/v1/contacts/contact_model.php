b23359ff3870ab8a1418d8d338a08b78d9075386
Marcus Hill <bcf8c9f2@opayq.com>
2022-09-11 11:08:48 +0100

    Adjust contact API to allow department to be text rather than int due to db schema change

--- api/v1/contacts/contact_model.php
+++ api/v1/contacts/contact_model.php
@@ -3,7 +3,7 @@ define('number_regex', '/[^0-9]/');
 
 $name = trim(strip_tags(mysqli_real_escape_string($mysqli,$_POST['contact_name'])));
 $title = trim(strip_tags(mysqli_real_escape_string($mysqli,$_POST['contact_title'])));
-$department = intval($_POST['contact_department']);
+$department = trim(strip_tags(mysqli_real_escape_string($mysqli,$_POST['contact_department'])));
 $phone = preg_replace(number_regex, '', $_POST['contact_phone']);
 $extension = preg_replace(number_regex, '', $_POST['contact_extension']);
 $mobile = preg_replace(number_regex, '', $_POST['contact_mobile']);
