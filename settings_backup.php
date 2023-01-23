a340df0df9c43d8682f8cb6802434e33fd3adffa
Marcus Hill <bcf8c9f2@opayq.com>
2023-01-23 21:37:32 +0000

    More general code cleanup/formatting

--- settings_backup.php
+++ settings_backup.php
@@ -1,33 +1,33 @@
 <?php require_once("inc_all_settings.php"); ?>
 
-<div class="card card-dark mb-3">
-    <div class="card-header py-3">
-        <h3 class="card-title"><i class="fa fa-fw fa-database"></i> Download Database</h3>
+    <div class="card card-dark mb-3">
+        <div class="card-header py-3">
+            <h3 class="card-title"><i class="fa fa-fw fa-database"></i> Download Database</h3>
+        </div>
+        <div class="card-body">
+            <center>
+                <a class="btn btn-primary btn-lg p-3" href="post.php?download_database"><i class="fa fa-fw fa-4x fa-download"></i><br><br>Download</a>
+            </center>
+        </div>
     </div>
-    <div class="card-body">
-        <center>
-            <a class="btn btn-primary btn-lg p-3" href="post.php?download_database"><i class="fa fa-fw fa-4x fa-download"></i><br><br>Download</a>
-        </center>
-    </div>
-</div>
 
-<div class="card card-dark">
-    <div class="card-header py-3">
-        <h3 class="card-title"><i class="fa fa-fw fa-key"></i> Backup Master Encryption Key</h3>
-    </div>
-    <div class="card-body">
-        <center>
-            <form action="post.php" method="POST">
-                <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token'] ?>">
-                <div class="input-group col-4">
-                    <div class="input-group-prepend">
-                        <input type="password" class="form-control" placeholder="Enter your account password" name="password" autocomplete="new-password" required>
+    <div class="card card-dark">
+        <div class="card-header py-3">
+            <h3 class="card-title"><i class="fa fa-fw fa-key"></i> Backup Master Encryption Key</h3>
+        </div>
+        <div class="card-body">
+            <center>
+                <form action="post.php" method="POST">
+                    <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token'] ?>">
+                    <div class="input-group col-4">
+                        <div class="input-group-prepend">
+                            <input type="password" class="form-control" placeholder="Enter your account password" name="password" autocomplete="new-password" required>
+                        </div>
+                        <button class="btn btn-primary" type="submit" name="backup_master_key"><i class="fa fa-fw fa-key"></i> Get Master Key</button>
                     </div>
-                    <button class="btn btn-primary" type="submit" name="backup_master_key"><i class="fa fa-fw fa-key"></i> Get Master Key</button>
-                </div>
-            </form>
-        </center>
+                </form>
+            </center>
+        </div>
     </div>
-</div>
 
 <?php include("footer.php");
--- settings_quote.php
+++ settings_quote.php
@@ -1,63 +1,63 @@
 <?php require_once("inc_all_settings.php"); ?>
 
-<div class="card card-dark">
-  <div class="card-header py-3">
-    <h3 class="card-title"><i class="fa fa-fw fa-file"></i> Quote Settings</h3>
-  </div>
-  <div class="card-body">
-    <form action="post.php" method="post" autocomplete="off">
-
-      <div class="form-group">
-        <label>Quote Prefix</label>
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <span class="input-group-text"><i class="fa fa-fw fa-barcode"></i></span>
-          </div>
-          <input type="text" class="form-control" name="config_quote_prefix" placeholder="Quote Prefix" value="<?php echo htmlentities($config_quote_prefix); ?>" required>
+    <div class="card card-dark">
+        <div class="card-header py-3">
+            <h3 class="card-title"><i class="fa fa-fw fa-file"></i> Quote Settings</h3>
         </div>
-      </div>
+        <div class="card-body">
+            <form action="post.php" method="post" autocomplete="off">
 
-      <div class="form-group">
-        <label>Next Number</label>
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <span class="input-group-text"><i class="fa fa-fw fa-barcode"></i></span>
-          </div>
-          <input type="number" min="0" class="form-control" name="config_quote_next_number" placeholder="Next Quote Number" value="<?php echo htmlentities($config_quote_next_number); ?>" required>
+                <div class="form-group">
+                    <label>Quote Prefix</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-fw fa-barcode"></i></span>
+                        </div>
+                        <input type="text" class="form-control" name="config_quote_prefix" placeholder="Quote Prefix" value="<?php echo htmlentities($config_quote_prefix); ?>" required>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Next Number</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-fw fa-barcode"></i></span>
+                        </div>
+                        <input type="number" min="0" class="form-control" name="config_quote_next_number" placeholder="Next Quote Number" value="<?php echo htmlentities($config_quote_next_number); ?>" required>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Quote Footer</label>
+                    <textarea class="form-control" rows="4" name="config_quote_footer"><?php echo htmlentities($config_quote_footer); ?></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label>From Email</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-fw fa-envelope"></i></span>
+                        </div>
+                        <input type="email" class="form-control" name="config_quote_from_email" placeholder="From Email" value="<?php echo htmlentities($config_quote_from_email); ?>">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>From Name</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-fw fa-tag"></i></span>
+                        </div>
+                        <input type="text" class="form-control" name="config_quote_from_name" placeholder="Name" value="<?php echo htmlentities($config_quote_from_name); ?>">
+                    </div>
+                </div>
+
+                <hr>
+
+                <button type="submit" name="edit_quote_settings" class="btn btn-primary text-bold"><i class="fa fa-check"></i> Save</button>
+
+            </form>
         </div>
-      </div>
-
-      <div class="form-group">
-        <label>Quote Footer</label>
-        <textarea class="form-control" rows="4" name="config_quote_footer"><?php echo htmlentities($config_quote_footer); ?></textarea>
-      </div>
-
-      <div class="form-group">
-        <label>From Email</label>
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <span class="input-group-text"><i class="fa fa-fw fa-envelope"></i></span>
-          </div>
-          <input type="email" class="form-control" name="config_quote_from_email" placeholder="From Email" value="<?php echo htmlentities($config_quote_from_email); ?>">
-        </div>
-      </div>
-
-      <div class="form-group">
-        <label>From Name</label>
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <span class="input-group-text"><i class="fa fa-fw fa-tag"></i></span>
-          </div>
-          <input type="text" class="form-control" name="config_quote_from_name" placeholder="Name" value="<?php echo htmlentities($config_quote_from_name); ?>">
-        </div>
-      </div>
-    
-      <hr>
-
-      <button type="submit" name="edit_quote_settings" class="btn btn-primary text-bold"><i class="fa fa-check"></i> Save</button>        
-    
-    </form>
-  </div>
-</div>
+    </div>
 
 <?php include("footer.php");
