b98439d74a53d30ecc59070f3bdf319791a59567
Marcus Hill <bcf8c9f2@opayq.com>
2023-01-25 22:59:28 +0000

    Remove unused  variable assignment.  was calling get_os with no parameters, so nothing was ever returned anyway. Get_os instead can now be given the user agent to determine the OS

--- check_login.php
+++ check_login.php
@@ -24,7 +24,6 @@ if (!isset($_SESSION['logged']) || !$_SESSION['logged']) {
 
 // SESSION FINGERPRINT
 $session_ip = strip_tags(mysqli_real_escape_string($mysqli,get_ip()));
-//$session_os = strip_tags(mysqli_real_escape_string($mysqli,get_os()));
 
 // User agent
 $session_user_agent = strip_tags(mysqli_real_escape_string($mysqli,$_SERVER['HTTP_USER_AGENT']));
