3dc1f11a56839909c4b5333940bd09b8543e7629
johnnyq <johnny@pittpc.com>
2023-01-24 20:08:37 -0500

    Added Basic Telemetry sending

--- settings_telemetry.php
+++ settings_telemetry.php
@@ -14,16 +14,19 @@
             <span class="input-group-text"><i class="fa fa-fw fa-broadcast-tower"></i></span>
           </div>
           <select class="form-control" name="config_telemetry">
-            <option <?php if ($config_telemetry == '0') { echo "selected"; } ?> value="0">Disabled</option>
-            <option <?php if ($config_telemetry == '1') { echo "selected"; } ?> value="1">Basic</option>
-            <option <?php if ($config_telemetry == '2') { echo "selected"; } ?> value="2">Detailed</option>
+            <option <?php if ($config_telemetry == "0") { echo "selected"; } ?> value = "0">Disabled</option>
+            <option <?php if ($config_telemetry == "1") { echo "selected"; } ?> value = "1">Basic</option>
+            <option <?php if ($config_telemetry == "2") { echo "selected"; } ?> value = "2">Detailed</option>
           </select>
         </div>
       </div>
 
       <hr>
       
-      <button type="submit" name="edit_telemetry_settings" class="btn btn-primary text-bold"><i class="fa fa-check"></i> Save</button>
+      <?php if ($config_telemetry > 0) { ?>
+      <button type="submit" name="send_telemetry_data" class="btn btn-success"><i class="fa fa-fw fa-paper-plane"></i> Send Telemetry Data</button>
+      <?php } ?>
+      <button type="submit" name="edit_telemetry_settings" class="btn btn-primary text-bold float-right"><i class="fa fa-check"></i> Save</button>
         
     </form>
   </div>
