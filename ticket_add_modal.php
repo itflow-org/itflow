31e8298876db73ee59598e7589af7ce5b170ee59
Marcus Hill <bcf8c9f2@opayq.com>
2023-01-29 22:40:25 +0000

    Tidy spacing

--- ticket_add_modal.php
+++ ticket_add_modal.php
@@ -11,22 +11,22 @@
                 <div class="modal-body bg-white">
 
                     <?php if (isset($_GET['client_id'])) { ?>
-                    <ul class="nav nav-pills nav-justified mb-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" data-toggle="pill" href="#pills-details"><i class="fa fa-fw fa-life-ring"></i> Details</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="pill" href="#pills-contacts"><i class="fa fa-fw fa-users"></i> Contacts</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="pill" href="#pills-assets"><i class="fa fa-fw fa-desktop"></i> Assets</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="pill" href="#pills-vendors"><i class="fa fa-fw fa-building"></i> Vendors</a>
-                        </li>              
-                    </ul>
+                        <ul class="nav nav-pills nav-justified mb-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" data-toggle="pill" href="#pills-details"><i class="fa fa-fw fa-life-ring"></i> Details</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="pill" href="#pills-contacts"><i class="fa fa-fw fa-users"></i> Contacts</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="pill" href="#pills-assets"><i class="fa fa-fw fa-desktop"></i> Assets</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="pill" href="#pills-vendors"><i class="fa fa-fw fa-building"></i> Vendors</a>
+                            </li>
+                        </ul>
 
-                    <hr>
+                        <hr>
 
                     <?php } ?>
 
@@ -49,27 +49,27 @@
                             </div>
 
                             <?php if (empty($_GET['client_id'])) { ?>
-                            
-                            <div class="form-group">
-                                <label>Client <strong class="text-danger">*</strong></label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="fa fa-fw fa-user"></i></span>
+
+                                <div class="form-group">
+                                    <label>Client <strong class="text-danger">*</strong></label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-fw fa-user"></i></span>
+                                        </div>
+                                        <select class="form-control select2" name="client" required>
+                                            <option value="">- Client -</option>
+                                            <?php
+
+                                            $sql = mysqli_query($mysqli,"SELECT * FROM clients WHERE company_id = $session_company_id ORDER BY client_name ASC");
+                                            while ($row = mysqli_fetch_array($sql)) {
+                                                $client_id = $row['client_id'];
+                                                $client_name = htmlentities($row['client_name']); ?>
+                                                <option value="<?php echo $client_id; ?>"><?php echo "$client_name"; ?></option>
+
+                                            <?php } ?>
+                                        </select>
                                     </div>
-                                    <select class="form-control select2" name="client" required>
-                                        <option value="">- Client -</option>
-                                        <?php
-
-                                        $sql = mysqli_query($mysqli,"SELECT * FROM clients WHERE company_id = $session_company_id ORDER BY client_name ASC");
-                                        while ($row = mysqli_fetch_array($sql)) {
-                                            $client_id = $row['client_id'];
-                                            $client_name = htmlentities($row['client_name']); ?>
-                                            <option value="<?php echo $client_id; ?>"><?php echo "$client_name"; ?></option>
-
-                                        <?php } ?>
-                                    </select>
                                 </div>
-                            </div>
 
                             <?php } ?>
 
@@ -116,80 +116,80 @@
 
                         <?php if (isset($_GET['client_id'])) { ?>
 
-                        <div class="tab-pane fade" id="pills-contacts">
+                            <div class="tab-pane fade" id="pills-contacts">
 
-                            <input type="hidden" name="client" value="<?php echo $client_id; ?>">
-                            <div class="form-group">
-                                <label>Contact <strong class="text-danger">*</strong></label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="fa fa-fw fa-user"></i></span>
-                                    </div>
-                                    <select class="form-control select2" name="contact" required>
-                                        <option value="">- Contact -</option>
-                                        <?php
-                                        $sql = mysqli_query($mysqli,"SELECT * FROM contacts WHERE contact_client_id = $client_id AND contact_archived_at IS NULL ORDER BY contact_name ASC");
-                                        while ($row = mysqli_fetch_array($sql)) {
-                                            $contact_id = $row['contact_id'];
-                                            $contact_name = htmlentities($row['contact_name']); ?>
-                                            <option value="<?php echo $contact_id; ?>" <?php if ($primary_contact == $contact_id) { echo "selected"; } ?>><?php echo "$contact_name"; ?></option>
+                                <input type="hidden" name="client" value="<?php echo $client_id; ?>">
+                                <div class="form-group">
+                                    <label>Contact <strong class="text-danger">*</strong></label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-fw fa-user"></i></span>
+                                        </div>
+                                        <select class="form-control select2" name="contact" required>
+                                            <option value="">- Contact -</option>
+                                            <?php
+                                            $sql = mysqli_query($mysqli,"SELECT * FROM contacts WHERE contact_client_id = $client_id AND contact_archived_at IS NULL ORDER BY contact_name ASC");
+                                            while ($row = mysqli_fetch_array($sql)) {
+                                                $contact_id = $row['contact_id'];
+                                                $contact_name = htmlentities($row['contact_name']); ?>
+                                                <option value="<?php echo $contact_id; ?>" <?php if ($primary_contact == $contact_id) { echo "selected"; } ?>><?php echo "$contact_name"; ?></option>
 
                                             <?php } ?>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="tab-pane fade" id="pills-assets">
-
-                            <div class="form-group">
-                                <label>Asset</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="fa fa-fw fa-desktop"></i></span>
+                                        </select>
                                     </div>
-                                    <select class="form-control select2" name="asset">
-                                        <option value="0">- None -</option>
-                                        <?php
-
-                                        $sql_assets = mysqli_query($mysqli,"SELECT * FROM assets WHERE asset_client_id = $client_id AND asset_archived_at IS NULL ORDER BY asset_name ASC");
-                                        while ($row = mysqli_fetch_array($sql_assets)) {
-                                            $asset_id_select = $row['asset_id'];
-                                            $asset_name_select = htmlentities($row['asset_name']); ?>    
-                                            <option value="<?php echo $asset_id_select; ?>"><?php echo $asset_name_select; ?></option>
-
-                                        <?php } ?>
-                                    </select>
                                 </div>
                             </div>
-                            
-                        </div>
 
-                        <div class="tab-pane fade" id="pills-vendors">
+                            <div class="tab-pane fade" id="pills-assets">
 
-                            <div class="form-group">
-                                <label>Vendor</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="fa fa-fw fa-building"></i></span>
+                                <div class="form-group">
+                                    <label>Asset</label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-fw fa-desktop"></i></span>
+                                        </div>
+                                        <select class="form-control select2" name="asset">
+                                            <option value="0">- None -</option>
+                                            <?php
+
+                                            $sql_assets = mysqli_query($mysqli,"SELECT * FROM assets WHERE asset_client_id = $client_id AND asset_archived_at IS NULL ORDER BY asset_name ASC");
+                                            while ($row = mysqli_fetch_array($sql_assets)) {
+                                                $asset_id_select = $row['asset_id'];
+                                                $asset_name_select = htmlentities($row['asset_name']); ?>
+                                                <option value="<?php echo $asset_id_select; ?>"><?php echo $asset_name_select; ?></option>
+
+                                            <?php } ?>
+                                        </select>
                                     </div>
-                                    <select class="form-control select2" name="vendor">
-                                        <option value="0">- None -</option>
-                                        <?php
-
-                                        $sql_vendors = mysqli_query($mysqli,"SELECT * FROM vendors WHERE vendor_client_id = $client_id AND vendor_template = 0 AND vendor_archived_at IS NULL ORDER BY vendor_name ASC");
-                                        while ($row = mysqli_fetch_array($sql_vendors)) {
-                                            $vendor_id_select = $row['vendor_id'];
-                                            $vendor_name_select = htmlentities($row['vendor_name']); ?>    
-                                            <option value="<?php echo $vendor_id_select; ?>"><?php echo $vendor_name_select; ?></option>
-
-                                        <?php } ?>
-                                    </select>
                                 </div>
+
                             </div>
-                            
-                        </div>
-                        
+
+                            <div class="tab-pane fade" id="pills-vendors">
+
+                                <div class="form-group">
+                                    <label>Vendor</label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fa fa-fw fa-building"></i></span>
+                                        </div>
+                                        <select class="form-control select2" name="vendor">
+                                            <option value="0">- None -</option>
+                                            <?php
+
+                                            $sql_vendors = mysqli_query($mysqli,"SELECT * FROM vendors WHERE vendor_client_id = $client_id AND vendor_template = 0 AND vendor_archived_at IS NULL ORDER BY vendor_name ASC");
+                                            while ($row = mysqli_fetch_array($sql_vendors)) {
+                                                $vendor_id_select = $row['vendor_id'];
+                                                $vendor_name_select = htmlentities($row['vendor_name']); ?>
+                                                <option value="<?php echo $vendor_id_select; ?>"><?php echo $vendor_name_select; ?></option>
+
+                                            <?php } ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                            </div>
+
                         <?php } ?>
 
                     </div>
