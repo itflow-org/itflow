cb14efc9b0d0ee7d43dfe493dfdb59d0228ad117
johnnyq <johnny@pittpc.com>
2023-01-30 18:24:16 -0500

    If Send Telemetry is clicked then send data no need to check if telemetry is enabled if button is selected

--- post.php
+++ post.php
@@ -1129,10 +1129,7 @@ if(isset($_POST['send_telemetry_data'])){
 
     validateAdminRole();
 
-    $config_telemetry = intval($_POST['config_telemetry']);
     $comments = $_POST['comments'];
-
-    mysqli_query($mysqli,"UPDATE settings SET config_telemetry = $config_telemetry WHERE company_id = $session_company_id");
 
     $sql = mysqli_query($mysqli,"SELECT * FROM companies LIMIT 1");
     $row = mysqli_fetch_array($sql);
@@ -1144,170 +1141,165 @@ if(isset($_POST['send_telemetry_data'])){
     $currency = $row['company_currency'];
     $current_version = exec("git rev-parse HEAD");
 
-    // Basic Telemetry
-    if($config_telemetry == 1){
+    // Client Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('client_id') AS num FROM clients"));
+    $client_count = $row['num'];
 
-        // Client Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('client_id') AS num FROM clients"));
-        $client_count = $row['num'];
+    // Ticket Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('recurring_id') AS num FROM tickets"));
+    $ticket_count = $row['num'];
 
-        // Ticket Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('recurring_id') AS num FROM tickets"));
-        $ticket_count = $row['num'];
+    // Calendar Event Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('event_id') AS num FROM events"));
+    $calendar_event_count = $row['num'];
 
-        // Calendar Event Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('event_id') AS num FROM events"));
-        $calendar_event_count = $row['num'];
+    // Quote Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('quote_id') AS num FROM quotes"));
+    $quote_count = $row['num'];
 
-        // Quote Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('quote_id') AS num FROM quotes"));
-        $quote_count = $row['num'];
+    // Invoice Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('invoice_id') AS num FROM invoices"));
+    $invoice_count = $row['num'];  
 
-        // Invoice Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('invoice_id') AS num FROM invoices"));
-        $invoice_count = $row['num'];  
+    // Revenue Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('revenue_id') AS num FROM revenues"));
+    $revenue_count = $row['num'];  
 
-        // Revenue Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('revenue_id') AS num FROM revenues"));
-        $revenue_count = $row['num'];  
+    // Recurring Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('recurring_id') AS num FROM recurring"));
+    $recurring_count = $row['num'];
 
-        // Recurring Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('recurring_id') AS num FROM recurring"));
-        $recurring_count = $row['num'];
+    // Account Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('account_id') AS num FROM accounts"));
+    $account_count = $row['num'];
 
-        // Account Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('account_id') AS num FROM accounts"));
-        $account_count = $row['num'];
+    // Tax Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('tax_id') AS num FROM taxes"));
+    $tax_count = $row['num'];
 
-        // Tax Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('tax_id') AS num FROM taxes"));
-        $tax_count = $row['num'];
+    // Product Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('product_id') AS num FROM products"));
+    $product_count = $row['num'];
 
-        // Product Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('product_id') AS num FROM products"));
-        $product_count = $row['num'];
+    // Payment Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('payment_id') AS num FROM payments WHERE payment_invoice_id > 0"));
+    $payment_count = $row['num'];
+    
+    // Company Vendor Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('vendor_id') AS num FROM vendors WHERE vendor_template = 0 AND vendor_client_id = 0"));
+    $company_vendor_count = $row['num'];
 
-        // Payment Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('payment_id') AS num FROM payments WHERE payment_invoice_id > 0"));
-        $payment_count = $row['num'];
-        
-        // Company Vendor Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('vendor_id') AS num FROM vendors WHERE vendor_template = 0 AND vendor_client_id = 0"));
-        $company_vendor_count = $row['num'];
+    // Expense Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('expense_id') AS num FROM expenses WHERE expense_vendor_id > 0"));
+    $expense_count = $row['num'];
 
-        // Expense Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('expense_id') AS num FROM expenses WHERE expense_vendor_id > 0"));
-        $expense_count = $row['num'];
+    // Trip Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('trip_id') AS num FROM trips"));
+    $trip_count = $row['num'];
 
-        // Trip Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('trip_id') AS num FROM trips"));
-        $trip_count = $row['num'];
+    // Transfer Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('transfer_id') AS num FROM transfers"));
+    $transfer_count = $row['num'];
 
-        // Transfer Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('transfer_id') AS num FROM transfers"));
-        $transfer_count = $row['num'];
+    // Contact Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('contact_id') AS num FROM contacts"));
+    $contact_count = $row['num'];
 
-        // Contact Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('contact_id') AS num FROM contacts"));
-        $contact_count = $row['num'];
+    // Location Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('location_id') AS num FROM locations"));
+    $location_count = $row['num'];
 
-        // Location Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('location_id') AS num FROM locations"));
-        $location_count = $row['num'];
+    // Asset Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('asset_id') AS num FROM assets"));
+    $asset_count = $row['num'];
 
-        // Asset Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('asset_id') AS num FROM assets"));
-        $asset_count = $row['num'];
+    // Software Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('software_id') AS num FROM software WHERE software_template = 0"));
+    $software_count = $row['num'];
 
-        // Software Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('software_id') AS num FROM software WHERE software_template = 0"));
-        $software_count = $row['num'];
+    // Software Template Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('software_id') AS num FROM software WHERE software_template = 1"));
+    $software_template_count = $row['num'];
 
-        // Software Template Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('software_id') AS num FROM software WHERE software_template = 1"));
-        $software_template_count = $row['num'];
+    // Password Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('login_id') AS num FROM logins"));
+    $password_count = $row['num'];
 
-        // Password Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('login_id') AS num FROM logins"));
-        $password_count = $row['num'];
+    // Network Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('network_id') AS num FROM networks"));
+    $network_count = $row['num'];
 
-        // Network Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('network_id') AS num FROM networks"));
-        $network_count = $row['num'];
+    // Certificate Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('certificate_id') AS num FROM certificates"));
+    $certificate_count = $row['num'];
 
-        // Certificate Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('certificate_id') AS num FROM certificates"));
-        $certificate_count = $row['num'];
+    // Domain Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('domain_id') AS num FROM domains"));
+    $domain_count = $row['num'];
 
-        // Domain Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('domain_id') AS num FROM domains"));
-        $domain_count = $row['num'];
+    // Service Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('service_id') AS num FROM services"));
+    $service_count = $row['num'];
 
-        // Service Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('service_id') AS num FROM services"));
-        $service_count = $row['num'];
+    // Client Vendor Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('vendor_id') AS num FROM vendors WHERE vendor_template = 0 AND vendor_client_id > 0"));
+    $client_vendor_count = $row['num'];
 
-        // Client Vendor Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('vendor_id') AS num FROM vendors WHERE vendor_template = 0 AND vendor_client_id > 0"));
-        $client_vendor_count = $row['num'];
+    // Vendor Template Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('vendor_id') AS num FROM vendors WHERE vendor_template = 1"));
+    $vendor_template_count = $row['num'];
 
-        // Vendor Template Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('vendor_id') AS num FROM vendors WHERE vendor_template = 1"));
-        $vendor_template_count = $row['num'];
+    // File Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('file_id') AS num FROM files"));
+    $file_count = $row['num'];
 
-        // File Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('file_id') AS num FROM files"));
-        $file_count = $row['num'];
+    // Document Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('document_id') AS num FROM documents WHERE document_template = 0"));
+    $document_count = $row['num'];
 
-        // Document Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('document_id') AS num FROM documents WHERE document_template = 0"));
-        $document_count = $row['num'];
+    // Document Template Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('document_id') AS num FROM documents WHERE document_template = 1"));
+    $document_template_count = $row['num'];       
 
-        // Document Template Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('document_id') AS num FROM documents WHERE document_template = 1"));
-        $document_template_count = $row['num'];       
+    // Shared Item Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('item_id') AS num FROM shared_items"));
+    $shared_item_count = $row['num'];
 
-        // Shared Item Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('item_id') AS num FROM shared_items"));
-        $shared_item_count = $row['num'];
+    // Company Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('company_id') AS num FROM companies"));
+    $company_count = $row['num'];
 
-        // Company Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('company_id') AS num FROM companies"));
-        $company_count = $row['num'];
+    // User Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('user_id') AS num FROM users"));
+    $user_count = $row['num'];
 
-        // User Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('user_id') AS num FROM users"));
-        $user_count = $row['num'];
+    // Category Expense Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('category_id') AS num FROM categories WHERE category_type = 'Expense'"));
+    $category_expense_count = $row['num'];
 
-        // Category Expense Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('category_id') AS num FROM categories WHERE category_type = 'Expense'"));
-        $category_expense_count = $row['num'];
+    // Category Income Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('category_id') AS num FROM categories WHERE category_type = 'Income'"));
+    $category_income_count = $row['num'];
 
-        // Category Income Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('category_id') AS num FROM categories WHERE category_type = 'Income'"));
-        $category_income_count = $row['num'];
+    // Category Referral Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('category_id') AS num FROM categories WHERE category_type = 'Referral'"));
+    $category_referral_count = $row['num'];
 
-        // Category Referral Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('category_id') AS num FROM categories WHERE category_type = 'Referral'"));
-        $category_referral_count = $row['num'];
+    // Category Payment Method Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('category_id') AS num FROM categories WHERE category_type = 'Payment Method'"));
+    $category_payment_method_count = $row['num'];
 
-        // Category Payment Method Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('category_id') AS num FROM categories WHERE category_type = 'Payment Method'"));
-        $category_payment_method_count = $row['num'];
+    // Tag Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('tag_id') AS num FROM tags"));
+    $tag_count = $row['num'];
 
-        // Tag Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('tag_id') AS num FROM tags"));
-        $tag_count = $row['num'];
+    // API Key Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('api_key_id') AS num FROM api_keys"));
+    $api_key_count = $row['num'];
 
-        // API Key Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('api_key_id') AS num FROM api_keys"));
-        $api_key_count = $row['num'];
-
-        // Log Count
-        $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('log_id') AS num FROM logs"));
-        $log_count = $row['num'];
-
-    }
+    // Log Count
+    $row = mysqli_fetch_assoc(mysqli_query($mysqli,"SELECT COUNT('log_id') AS num FROM logs"));
+    $log_count = $row['num'];
 
     $postdata = http_build_query(
       array(
