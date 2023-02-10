56fd25d20e9bf98e6ba653dd9508c0a4cf527694
johnnyq <johnny@pittpc.com>
2023-02-09 20:22:55 -0500

    Fix last commit from redirecting to login page when already logged in

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
-<?php
+<?php 
 
 if (file_exists("config.php")) {
-    header("Location: login.php");
+    include("inc_all.php"); ?>
+	<!-- Breadcrumbs-->
+	<ol class="breadcrumb">
+	  <li class="breadcrumb-item">
+	    <a href="index.php">Dashboard</a>
+	  </li>
+	  <li class="breadcrumb-item active">Blank Page</li>
+	</ol>
+
+	<!-- Page Content -->
+	<h1>Blank Page</h1>
+	<hr>
+	<?php 
+
+	include("footer.php");
 
 } else {
 	header("Location: setup.php");
