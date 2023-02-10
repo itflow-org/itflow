d978911b7d3433d4f269e643d36c5579dbb0f539
johnnyq <johnny@pittpc.com>
2023-02-09 20:13:57 -0500

    Updated index.php to act as a redirector if config.php exits redirect to login if it doesnt then goto setup

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<?php include("inc_all.php"); ?>
-	<!-- Breadcrumbs-->
-	<ol class="breadcrumb">
-	  <li class="breadcrumb-item">
-	    <a href="index.php">Dashboard</a>
-	  </li>
-	  <li class="breadcrumb-item active">Blank Page</li>
-	</ol>
+<?php
 
-	<!-- Page Content -->
-	<h1>Blank Page</h1>
-	<hr>
-<?php include("footer.php"); ?>
+if (file_exists("config.php")) {
+    header("Location: login.php");
+
+} else {
+	header("Location: setup.php");
+}
+
+?>
