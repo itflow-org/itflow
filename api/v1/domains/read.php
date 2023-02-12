6f7f679682c0bff879bf1d9990e3b649a2febdbc
Marcus Hill <bcf8c9f2@opayq.com>
2023-02-12 15:56:09 +0000

    Tidy

--- api/v1/domains/read.php
+++ api/v1/domains/read.php
@@ -7,25 +7,21 @@ require_once('../require_get_method.php');
 if (isset($_GET['domain_id'])) {
     $id = intval($_GET['domain_id']);
     $sql = mysqli_query($mysqli, "SELECT * FROM domains WHERE domain_id = '$id' AND domain_client_id LIKE '$client_id' AND company_id = '$company_id'");
-}
 
-
-elseif (isset($_GET['domain_name'])) {
+} elseif (isset($_GET['domain_name'])) {
     // Domain by name
 
     $name = mysqli_real_escape_string($mysqli, $_GET['domain_name']);
     $sql = mysqli_query($mysqli, "SELECT * FROM domains WHERE domain_name = '$name' AND domain_client_id LIKE '$client_id' AND company_id = '$company_id' ORDER BY asset_id LIMIT $limit OFFSET $offset");
-}
 
-
-elseif (isset($_GET['client_id'])) {
+} elseif (isset($_GET['client_id'])) {
     // Domain via client ID
 
     $sql = mysqli_query($mysqli, "SELECT * FROM domains WHERE domain_client_id LIKE '$client_id' AND company_id = '$company_id' ORDER BY domain_id LIMIT $limit OFFSET $offset");
-}
 
-// All domains
-else {
+} else {
+    // All domains
+
     $sql = mysqli_query($mysqli, "SELECT * FROM domains WHERE domain_client_id LIKE '$client_id' AND company_id = '$company_id' ORDER BY domain_id LIMIT $limit OFFSET $offset");
 }
 
