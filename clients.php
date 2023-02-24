6478af409fb05c39b85b776f2e077210cd9d3afd
taynted <55361574+taynted@users.noreply.github.com>
2023-02-24 12:16:17 -0500

    Update clients.php

--- clients.php
+++ clients.php
@@ -83,7 +83,7 @@ if ($_GET['canned_date'] == "custom" && !empty($_GET['date_from'])) {
     $date_to = date('Y-m-d', strtotime("last day of december last year"));
 } else {
     $date_from = "1000-01-01";
-    $date_to = "9999-01-01";
+    $date_to = "2999-12-31";
 }
 
 //Rebuild URL
