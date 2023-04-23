541d1dd66755bc7e0a7b9b84be7162a1d850beda
johnnyq <johnny@pittpc.com>
2023-04-23 14:27:45 -0400

    Add precautionary warning when exporting CSV data, still need to add modals for client data

---
+++ inc_export_warning.php
@@ -0,0 +1,4 @@
+<div class="alert alert-warning">
+    <h5>Warning</h5>
+    Be cautious with <strong>CSV</strong> data as it can contain <strong>malicious</strong> code that may execute in certain applications like <strong>Microsoft Office</strong>. Review data before exporting or opening such files.
+</div>
