cf0205ba2180e7e348c37500a0042f2cb85c07ea
johnnyq <johnny@pittpc.com>
2023-05-09 16:37:32 -0400

    Tidy Portal Header

--- portal/inc_portal.php
+++ portal/inc_portal.php
@@ -8,20 +8,4 @@ require_once('../config.php');
 require_once('../functions.php');
 require_once('check_login.php');
 require_once('portal_functions.php');
-
-if (!isset($_SESSION)) {
-    // HTTP Only cookies
-    ini_set("session.cookie_httponly", true);
-    if ($config_https_only) {
-        // Tell client to only send cookie(s) over HTTPS
-        ini_set("session.cookie_secure", true);
-    }
-    session_start();
-}
-
-// Get Company Information
-$sql = mysqli_query($mysqli, "SELECT * FROM companies WHERE company_id = 1");
-$row = mysqli_fetch_array($sql);
-$company_name = $row['company_name'];
-
 require_once("portal_header.php");
