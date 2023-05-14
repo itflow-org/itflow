1057481039c80b66dbb02811ccd993fa9c599fbb
Marcus Hill <bcf8c9f2@opayq.com>
2023-05-14 20:16:04 +0100

    Ticket.php
    - Change wording of ticket 'Respond' button to 'Add note' if reply is internal
    - Fix TinyMCE not loading under certain circumstances
    - Correct varname 'prefix' to 'ticket_prefix' in reply alert message

---
+++ js/ticket_button_respond_note.js
@@ -0,0 +1,19 @@
+// Ticket.php - Changes the wording of the "Respond" button to "Add note" if reply is not a public update (based on checkbox)
+
+// Get Internal/Public Checkbox
+checkbox = document.getElementById('ticket_reply_type_checkbox');
+
+// Get Respond button
+respond = document.getElementById('ticket_add_reply');
+
+// When checkbox is checked/unchecked, update button wording
+checkbox.addEventListener('change', e => {
+    if (e.target.checked) {
+        // Public reply
+        respond.innerHTML = "<i class=\"fas fa-check mr-2\"></i>Respond";
+
+    } else {
+        // Internal note
+        respond.innerHTML = "<i class=\"fas fa-sticky-note mr-2\"></i>Add note";
+    }
+});
