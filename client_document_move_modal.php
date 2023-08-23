12bbfd1db151b6e185d8299227ba7dfeae5632ed
johnnyq <johnny@pittpc.com>
2023-08-23 18:12:59 -0400

    Add select2 Class on folder

--- client_document_move_modal.php
+++ client_document_move_modal.php
@@ -18,7 +18,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fa fa-fw fa-folder"></i></span>
                             </div>
-                            <select class="form-control" name="folder">
+                            <select class="form-control select2" name="folder">
                                 <option value="0">/</option>
                                 <?php
                                 $sql_folders_select = mysqli_query($mysqli, "SELECT * FROM folders WHERE folder_location = $folder_location AND folder_client_id = $client_id ORDER BY folder_name ASC");
