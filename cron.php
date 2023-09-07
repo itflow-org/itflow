420647d48137e2790a24a1d290b4d08984d40552
johnnyq <johnny@pittpc.com>
2023-09-07 17:51:35 -0400

    Seperated domain refresher to seperate cron job this will need to be added manually if you want domain info to be refreshed, did this because sometimes it would break

--- cron.php
+++ cron.php
@@ -106,48 +106,6 @@ mysqli_query($mysqli, "DELETE FROM email_queue WHERE email_queued_at < CURDATE()
 //Logging
 mysqli_query($mysqli, "INSERT INTO logs SET log_type = 'Cron', log_action = 'Task', log_description = 'Cron cleaned up old data'");
 
-
-
-
-/*
- * ###############################################################################################################
- *  REFRESH DATA
- * ###############################################################################################################
- */
-// 2023-02-20 JQ Commenting this code out as its intermitently breaking cron executions, investigating
-// ERROR
-// php cron.php
-// PHP Fatal error:  Uncaught TypeError: mysqli_fetch_array(): Argument #1 ($result) must be of type mysqli_result, bool given in cron.php:141
-// Stack trace:
-//#0 cron.php(141): mysqli_fetch_array()
-//#1 {main}
-//  thrown in cron.php on line 141
-// END ERROR
-// REFRESH DOMAIN WHOIS DATA (1 a day)
-//  Get the oldest updated domain (MariaDB shows NULLs first when ordering by default)
-//$row = mysqli_fetch_array(mysqli_query($mysqli, "SELECT domain_id, domain_name FROM `domains` ORDER BY domain_updated_at LIMIT 1"));
-
-//if ($row) {
-//    $domain_id = intval($row['domain_id']);
-//    $domain_name = sanitizeInput($row['domain_name']);
-
-//    $expire = getDomainExpirationDate($domain_name);
-//    $records = getDomainRecords($domain_name);
-//    $a = sanitizeInput($records['a']);
-//    $ns = sanitizeInput($records['ns']);
-//    $mx = sanitizeInput($records['mx']);
-//    $txt = sanitizeInput($records['txt']);
-//    $whois = sanitizeInput($records['whois']);
-
-    // Update the domain
-//    mysqli_query($mysqli, "UPDATE domains SET domain_name = '$domain_name',  domain_expire = '$expire', domain_ip = '$a', domain_name_servers = '$ns', domain_mail_servers = '$mx', domain_txt = '$txt', domain_raw_whois = '$whois' WHERE domain_id = $domain_id");
-//}
-
-
-// TODO: Re-add the cert refresher
-
-
-
 /*
  * ###############################################################################################################
  *  ACTION DATA
