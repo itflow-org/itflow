6760997beb5522c342f5211f6e4352ea3e4e7587
johnnyq <johnny@pittpc.com>
2023-10-04 22:38:29 -0400

    Set Company Timezone properly upon login

--- check_login.php
+++ check_login.php
@@ -52,6 +52,10 @@ $session_company_name = $row['company_name'];
 $session_company_country = $row['company_country'];
 $session_company_locale = $row['company_locale'];
 $session_company_currency = $row['company_currency'];
+$session_company_timezone = $row['company_timezone'];
+
+// Set Timezone to the companies timezone
+date_default_timezone_set('$session_company_timezone');
 
 //Set Currency Format
 $currency_format = numfmt_create($session_company_locale, NumberFormatter::CURRENCY);
