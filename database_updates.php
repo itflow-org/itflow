29e2e5e0d71adcbe67354162709efc6b17e985ed
Andrew Malsbury <andrew@malsbury.tech>
2023-10-07 16:14:28 +0000

    Update DB, and added comments for next new guy.

--- database_updates.php
+++ database_updates.php
@@ -1374,12 +1374,26 @@ if (LATEST_DATABASE_VERSION > CURRENT_DATABASE_VERSION) {
         mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '0.8.6'");
     }
 
-    //if (CURRENT_DATABASE_VERSION == '0.8.6') {
+    // Update DB to 0.8.7
+    if (CURRENT_DATABASE_VERSION == '0.8.6') {
     // Insert queries here required to update to DB version 0.8.7
+    mysqli_query($mysqli, "ALTER TABLE `accounts` ADD `account_type` int(6) DEFAULT NULL AFTER `account_notes`");
 
     // Then, update the database to the next sequential version
-    //mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '0.8.7'");
+    mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '0.8.7'");
+    }
+
+    // Be sure to change database_version.php to reflect the version you are updating to here
+    // Please add this same comment block to the bottom of this file, and update the version number.
+    // Uncomment Below Lines, to add additional database updates
+    //
+    //if (CURRENT_DATABASE_VERSION == '0.8.7') {
+    // Insert queries here required to update to DB version 0.8.9
+    //
+    // Then, update the database to the next sequential version
+    //mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '0.8.8'");
     //}
+    //
 
 } else {
     // Up-to-date
