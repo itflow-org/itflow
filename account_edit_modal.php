db5eee0ebb2a06347811a2c198266b48cc231544
Andrew Malsbury <andrew@malsbury.tech>
2023-10-07 16:13:10 +0000

    Add type dropdown

--- account_edit_modal.php
+++ account_edit_modal.php
@@ -22,6 +22,25 @@
                     </div>
 
                     <div class="form-group">
+                        <label>Account Type <strong class="text-danger">*</strong></label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fa fa-fw fa-list"></i></span>
+                            </div>
+                            <select class="form-control select" name="type" required>
+                                <option value="">- Select -</option>
+                                <option value="11" <?php if ($account_type == 'Current Assets') echo 'selected'; ?>>Current Assets</option>
+                                <option value="12" <?php if ($account_type == 'Fixed Assets') echo 'selected'; ?>>Fixed Assets</option>
+                                <option value="13" <?php if ($account_type == 'Other Assets') echo 'selected'; ?>>Other Assets</option>
+                                <option value="21" <?php if ($account_type == 'Current Liabilities') echo 'selected'; ?>>Current Liabilities</option>
+                                <option value="22" <?php if ($account_type == 'Long Term Liabilities') echo 'selected'; ?>>Long Term Liabilities</option>
+                                <option value="23" <?php if ($account_type == 'Other Liabilities') echo 'selected'; ?>>Other Liabilities</option>
+                                <option value="30" <?php if ($account_type == 'Equity') echo 'selected'; ?>>Equity</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
                         <label>Notes</label>
                         <textarea class="form-control" rows="5" placeholder="Enter some notes" name="notes"><?php echo $account_notes; ?></textarea>
                     </div>
