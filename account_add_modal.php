6a485a95b46dca711f8796bffe7b382c40f5240d
Andrew Malsbury <andrew@malsbury.tech>
2023-10-07 17:32:34 +0000

    Allows for negative numbers for credit cards

--- account_add_modal.php
+++ account_add_modal.php
@@ -45,8 +45,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fa fa-fw fa-dollar-sign"></i></span>
                             </div>
-                            <input type="text" class="form-control" inputmode="numeric" pattern="[0-9]*\.?[0-9]{0,2}" name="opening_balance" placeholder="0.00" required>
-                        </div>
+                            <input type="text" class="form-control" inputmode="numeric" pattern="-?[0-9]*\.?[0-9]{0,2}" name="opening_balance" placeholder="0.00" required>
                     </div>
 
                     <div class="form-group">
