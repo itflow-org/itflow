3ef045eea19457b68e0259e8083d9ba2e1a8943a
Andrew Malsbury <andrew@malsbury.tech>
2023-10-09 20:28:12 +0000

    New Modal

---
+++ settings_account_types_add_modal.php
@@ -0,0 +1,37 @@
+<div class="modal" id="addAccountTypeModal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content bg-dark">
+            <div class="modal-header text-white">
+                <h5 class="modal-title"><i class="fas fa-money-bill-wave mr-2"></i>New Account Type</h5>
+                <button type="button" class="close text-white" data-dismiss="modal">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="post.php" method="post" autocomplete="off">
+                <div class="modal-body bg-white">
+                    <div class="form-group">
+                        <label>Name <strong class="text-danger">*</strong></label>
+                        <input type="text" class="form-control" name="name" placeholder="Account Name" required autofocus>
+                    </div>
+                    <div class="form-group">
+                        <label>Account Type</label>
+                        <select class="form-control select2" name="type">
+                            <option value="">- Select -</option>
+                            <option value="10">Assets</option>
+                            <option value="20">Liabilities</option>
+                            <option value="30">Equity</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label>Description</label>
+                        <textarea class="form-control" name="description" placeholder="Description"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer bg-white">
+                    <button type="submit" name="add_account_type" class="btn btn-primary text-bold"><i class="fa fa-check mr- 2"></i>Create</button>
+                    <button type="button" class="btn btn-light" data-dismiss="modal"><i class="fas fa-times mr-2"></i>Cancel</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
