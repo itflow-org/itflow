43786a72abfc8cbe100c6944ee60f4fa2d9923ad
johnnyq <johnny@pittpc.com>
2023-10-31 17:24:46 -0400

    Moved Advanced Filter button to the right for invoices and quotes

--- quotes.php
+++ quotes.php
@@ -38,9 +38,13 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
                         <div class="input-group">
                             <input type="search" class="form-control" name="q" value="<?php if (isset($q)) { echo stripslashes(nullable_htmlentities($q)); } ?>" placeholder="Search Quotes">
                             <div class="input-group-append">
-                                <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#advancedFilter"><i class="fas fa-filter"></i></button>
                                 <button class="btn btn-primary"><i class="fa fa-search"></i></button>
                             </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="float-right">
+                            <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#advancedFilter"><i class="fas fa-filter"></i></button>
                         </div>
                     </div>
                 </div>
