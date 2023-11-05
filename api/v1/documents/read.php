573eaf179b234bb34a87aa1b51148da72c5cfc53
Marcus Hill <bcf8c9f2@opayq.com>
2023-11-05 10:42:46 +0000

    API - Documents
    - Add create and update methods

--- api/v1/documents/read.php
+++ api/v1/documents/read.php
@@ -7,13 +7,15 @@ require_once '../require_get_method.php';
 
 if (isset($_GET['document_id'])) {
     // Document via ID (single)
-
     $id = intval($_GET['document_id']);
     $sql = mysqli_query($mysqli, "SELECT * FROM documents WHERE document_id = '$id' AND document_client_id LIKE '$client_id'");
 
+} elseif (isset($_GET['client_id'])) {
+    // Documents via client ID (multiple)
+    $sql = mysqli_query($mysqli, "SELECT * FROM documents WHERE document_client_id LIKE '$client_id' AND document_archived_at IS NULL");
+
 } else {
     // All documents
-
     $sql = mysqli_query($mysqli, "SELECT * FROM documents WHERE document_client_id LIKE '$client_id' ORDER BY document_id LIMIT $limit OFFSET $offset");
 }
 
