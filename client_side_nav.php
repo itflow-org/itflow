c18dbc5985a6f19d2c1605e26a3a1f83ac1091ac
johnnyq <johnny@pittpc.com>
2023-11-06 15:22:51 -0500

    Rename Events in Client Side Nav to Calendar

--- client_side_nav.php
+++ client_side_nav.php
@@ -97,7 +97,7 @@
                     <a href="client_events.php?client_id=<?php echo $client_id; ?>" class="nav-link <?php if (basename($_SERVER["PHP_SELF"]) == "client_events.php") { echo "active"; } ?>">
                         <i class="nav-icon fas fa-calendar-alt"></i>
                         <p>
-                            Events
+                            Calendar
                             <?php
                             if ($num_events > 0) { ?>
                                 <span class="right badge text-light"><?php echo $num_events; ?></span>
