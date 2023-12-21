78cb413d49cd4d8359ca80ca03ff05ec62801574
o-psi <andymalsb@gmail.com>
2023-12-21 14:29:58 +0000

    More reformatting

--- document_template_add_modal.php
+++ document_template_add_modal.php
@@ -20,13 +20,15 @@
                     <div class="form-group">
                         <input type="text" class="form-control" name="description" placeholder="Enter a short summary">
                     </div>
-                
+
                 </div>
 
                 <div class="modal-footer bg-white">
 
-                    <button type="submit" name="add_document_template" class="btn btn-primary text-bold"><i class="fa fa-check mr-2"></i>Create</button>
-                    <button type="button" class="btn btn-light" data-dismiss="modal"><i class="fa fa-times mr-2"></i>Cancel</button>
+                    <button type="submit" name="add_document_template" class="btn btn-primary text-bold"><i
+                            class="fa fa-check mr-2"></i>Create</button>
+                    <button type="button" class="btn btn-light" data-dismiss="modal"><i
+                            class="fa fa-times mr-2"></i>Cancel</button>
 
                 </div>
             </form>
--- transfers.php
+++ transfers.php
@@ -11,8 +11,17 @@ $url_query_strings_sort = http_build_query($get_copy);
 
 $sql = mysqli_query(
     $mysqli,
-    "SELECT SQL_CALC_FOUND_ROWS transfer_created_at, expense_date AS transfer_date, expense_amount AS transfer_amount, expense_account_id AS transfer_account_from, revenue_account_id AS transfer_account_to, transfer_expense_id, transfer_revenue_id , transfer_id, transfer_notes FROM transfers, expenses, revenues
-    WHERE transfer_expense_id = expense_id 
+    "SELECT SQL_CALC_FOUND_ROWS
+    transfer_created_at,
+    expense_date AS transfer_date,
+    expense_amount AS transfer_amount,
+    expense_account_id AS transfer_account_from,
+    revenue_account_id AS transfer_account_to,
+    transfer_expense_id, transfer_revenue_id ,
+    transfer_id, transfer_notes FROM transfers,
+    expenses,
+    revenues
+    WHERE transfer_expense_id = expense_id
     AND transfer_revenue_id = revenue_id
     AND DATE(expense_date) BETWEEN '$dtf' AND '$dtt'
     AND (transfer_notes LIKE '%$q%')
@@ -23,64 +32,82 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
 
 ?>
 
-    <div class="card card-dark">
-        <div class="card-header py-2">
-            <h3 class="card-title mt-2"><i class="fas fa-fw fa-exchange-alt mr-2"></i>Transfers</h3>
-            <div class="card-tools">
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addTransferModal"><i class="fas fa-plus mr-2"></i>New Transfer</button>
-            </div>
+<div class="card card-dark">
+    <div class="card-header py-2">
+        <h3 class="card-title mt-2"><i class="fas fa-fw fa-exchange-alt mr-2"></i>Transfers</h3>
+        <div class="card-tools">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addTransferModal"><i
+                    class="fas fa-plus mr-2"></i>New Transfer</button>
         </div>
+    </div>
 
-        <div class="card-body">
-            <form class="mb-4" autocomplete="off">
+    <div class="card-body">
+        <form class="mb-4" autocomplete="off">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="input-group">
+                        <input type="search" class="form-control" name="q"
+                            value="<?php if (isset($q)) { echo stripslashes(nullable_htmlentities($q)); } ?>"
+                            placeholder="Search Transfers">
+                        <div class="input-group-append">
+                            <button class="btn btn-secondary" type="button" data-toggle="collapse"
+                                data-target="#advancedFilter"><i class="fas fa-filter"></i></button>
+                            <button class="btn btn-primary"><i class="fa fa-search"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="collapse mt-3 <?php if (!empty($_GET['dtf']) || $_GET['canned_date'] !== "custom" ) {
+                echo "show";
+                } ?>"
+                id="advancedFilter">
                 <div class="row">
-                    <div class="col-sm-4">
-                        <div class="input-group">
-                            <input type="search" class="form-control" name="q" value="<?php if (isset($q)) { echo stripslashes(nullable_htmlentities($q)); } ?>" placeholder="Search Transfers">
-                            <div class="input-group-append">
-                                <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#advancedFilter"><i class="fas fa-filter"></i></button>
-                                <button class="btn btn-primary"><i class="fa fa-search"></i></button>
-                            </div>
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label>Canned Date</label>
+                            <select class="form-control select2" name="canned_date">
+                                <option <?php if ($_GET['canned_date'] == "custom") { echo "selected"; } ?>
+                                    value="custom">Custom</option>
+                                <option <?php if ($_GET['canned_date'] == "today") { echo "selected"; } ?>
+                                    value="today">Today</option>
+                                <option <?php if ($_GET['canned_date'] == "yesterday") { echo "selected"; } ?>
+                                    value="yesterday">Yesterday</option>
+                                <option <?php if ($_GET['canned_date'] == "thisweek") { echo "selected"; } ?>
+                                    value="thisweek">This Week</option>
+                                <option <?php if ($_GET['canned_date'] == "lastweek") { echo "selected"; } ?>
+                                    value="lastweek">Last Week</option>
+                                <option <?php if ($_GET['canned_date'] == "thismonth") { echo "selected"; } ?>
+                                    value="thismonth">This Month</option>
+                                <option <?php if ($_GET['canned_date'] == "lastmonth") { echo "selected"; } ?>
+                                    value="lastmonth">Last Month</option>
+                                <option <?php if ($_GET['canned_date'] == "thisyear") { echo "selected"; } ?>
+                                    value="thisyear">This Year</option>
+                                <option <?php if ($_GET['canned_date'] == "lastyear") { echo "selected"; } ?>
+                                    value="lastyear">Last Year</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label>Date From</label>
+                            <input type="date" class="form-control" name="dtf" max="2999-12-31"
+                                value="<?php echo nullable_htmlentities($dtf); ?>">
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label>Date To</label>
+                            <input type="date" class="form-control" name="dtt" max="2999-12-31"
+                                value="<?php echo nullable_htmlentities($dtt); ?>">
                         </div>
                     </div>
                 </div>
-                <div class="collapse mt-3 <?php if (!empty($_GET['dtf']) || $_GET['canned_date'] !== "custom" ) { echo "show"; } ?>" id="advancedFilter">
-                    <div class="row">
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label>Canned Date</label>
-                                <select class="form-control select2" name="canned_date">
-                                    <option <?php if ($_GET['canned_date'] == "custom") { echo "selected"; } ?> value="custom">Custom</option>
-                                    <option <?php if ($_GET['canned_date'] == "today") { echo "selected"; } ?> value="today">Today</option>
-                                    <option <?php if ($_GET['canned_date'] == "yesterday") { echo "selected"; } ?> value="yesterday">Yesterday</option>
-                                    <option <?php if ($_GET['canned_date'] == "thisweek") { echo "selected"; } ?> value="thisweek">This Week</option>
-                                    <option <?php if ($_GET['canned_date'] == "lastweek") { echo "selected"; } ?> value="lastweek">Last Week</option>
-                                    <option <?php if ($_GET['canned_date'] == "thismonth") { echo "selected"; } ?> value="thismonth">This Month</option>
-                                    <option <?php if ($_GET['canned_date'] == "lastmonth") { echo "selected"; } ?> value="lastmonth">Last Month</option>
-                                    <option <?php if ($_GET['canned_date'] == "thisyear") { echo "selected"; } ?> value="thisyear">This Year</option>
-                                    <option <?php if ($_GET['canned_date'] == "lastyear") { echo "selected"; } ?> value="lastyear">Last Year</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label>Date From</label>
-                                <input type="date" class="form-control" name="dtf" max="2999-12-31" value="<?php echo nullable_htmlentities($dtf); ?>">
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label>Date To</label>
-                                <input type="date" class="form-control" name="dtt" max="2999-12-31" value="<?php echo nullable_htmlentities($dtt); ?>">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <hr>
-            <div class="table-responsive-sm">
-                <table class="table table-striped table-borderless table-hover">
-                    <thead class="text-dark <?php if ($num_rows[0] == 0) { echo "d-none"; } ?>">
+            </div>
+        </form>
+        <hr>
+        <div class="table-responsive-sm">
+            <table class="table table-striped table-borderless table-hover">
+                <thead class="text-dark <?php if ($num_rows[0] == 0) { echo "d-none"; } ?>">
                     <tr>
                         <?php
                         $transferColumns = [
@@ -93,14 +120,15 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
 
                         foreach ($transferColumns as $sortParam => $columnName) {
                             $class = ($sortParam === 'transfer_amount') ? 'text-right' : '';
-                            echo "<th class='$class'><a class='text-dark' href='?$url_query_strings_sort&sort=$sortParam&order=$disp'>$columnName</a></th>";
+                            echo "<th class='$class'><a class='text-dark'
+                            href='?$url_query_strings_sort&sort=$sortParam&order=$disp'>$columnName</a></th>";
                         }
                         ?>
                         <th class="text-center">Action</th>
                     </tr>
 
-                    </thead>
-                    <tbody>
+                </thead>
+                <tbody>
                     <?php
 
                     while ($row = mysqli_fetch_array($sql)) {
@@ -119,7 +147,11 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
                         $expense_id = intval($row['transfer_expense_id']);
                         $revenue_id = intval($row['transfer_revenue_id']);
 
-                        $sql_from = mysqli_query($mysqli, "SELECT * FROM accounts WHERE account_id = $transfer_account_from");
+                        $sql_from = mysqli_query(
+                            $mysqli,
+                            "SELECT * FROM accounts
+                            WHERE account_id = $transfer_account_from
+                            ");
                         $row = mysqli_fetch_array($sql_from);
                         $account_name_from = nullable_htmlentities($row['account_name']);
                         $account_from_archived_at = nullable_htmlentities($row['account_archived_at']);
@@ -129,7 +161,11 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
                             $account_from_archived_display = "Archived - ";
                         }
 
-                        $sql_to = mysqli_query($mysqli, "SELECT * FROM accounts WHERE account_id = $transfer_account_to");
+                        $sql_to = mysqli_query(
+                            $mysqli,
+                            "SELECT * FROM accounts
+                            WHERE account_id = $transfer_account_to
+                            ");
                         $row = mysqli_fetch_array($sql_to);
                         $account_name_to = nullable_htmlentities($row['account_name']);
                         $account_to_archived_at = nullable_htmlentities($row['account_archived_at']);
@@ -140,31 +176,43 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
                         }
 
                         ?>
-                        <tr>
-                            <td><a class="text-dark" href="#" data-toggle="modal" data-target="#editTransferModal<?php echo $transfer_id; ?>"><?php echo $transfer_date; ?></a></td>
-                            <td><?php echo "$account_from_archived_display$account_name_from"; ?></td>
-                            <td><?php echo "$account_to_archived_display$account_name_to"; ?></td>
-                            <td><?php echo $transfer_notes_display; ?></td>
-                            <td class="text-bold text-right"><?php echo numfmt_format_currency($currency_format, $transfer_amount, $session_company_currency); ?></td>
-                            <td>
-                                <div class="dropdown dropleft text-center">
-                                    <button class="btn btn-secondary btn-sm" type="button" data-toggle="dropdown">
-                                        <i class="fas fa-ellipsis-h"></i>
-                                    </button>
-                                    <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#editTransferModal<?php echo $transfer_id; ?>">
-                                            <i class="fas fa-fw fa-edit mr-2"></i>Edit
-                                        </a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item text-danger text-bold confirm-link" href="post.php?delete_transfer=<?php echo $transfer_id; ?>">
-                                            <i class="fas fa-fw fa-trash mr-2"></i>Delete
-                                        </a>
-                                    </div>
+                    <tr>
+                        <td><a class="text-dark" href="#" data-toggle="modal"
+                                data-target="#editTransferModal<?php echo $transfer_id; ?>">
+                                <?php echo $transfer_date; ?>
+                            </a>
+                        </td>
+                        <td><?php echo "$account_from_archived_display$account_name_from"; ?></td>
+                        <td><?php echo "$account_to_archived_display$account_name_to"; ?></td>
+                        <td><?php echo $transfer_notes_display; ?></td>
+                        <td class="text-bold text-right">
+                            <?php echo numfmt_format_currency(
+                                $currency_format,
+                                $transfer_amount,
+                                $session_company_currency
+                                ); ?>
+                        </td>
+                        <td>
+                            <div class="dropdown dropleft text-center">
+                                <button class="btn btn-secondary btn-sm" type="button" data-toggle="dropdown">
+                                    <i class="fas fa-ellipsis-h"></i>
+                                </button>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#" data-toggle="modal"
+                                        data-target="#editTransferModal<?php echo $transfer_id; ?>">
+                                        <i class="fas fa-fw fa-edit mr-2"></i>Edit
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item text-danger text-bold confirm-link"
+                                        href="post.php?delete_transfer=<?php echo $transfer_id; ?>">
+                                        <i class="fas fa-fw fa-trash mr-2"></i>Delete
+                                    </a>
                                 </div>
-                            </td>
-                        </tr>
+                            </div>
+                        </td>
+                    </tr>
 
-                        <?php
+                    <?php
 
                         require "transfer_edit_modal.php";
 
@@ -173,16 +221,15 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
 
                     ?>
 
-                    </tbody>
-                </table>
-            </div>
-            <?php require_once "pagination.php";
- ?>
+                </tbody>
+            </table>
         </div>
+        <?php require_once "pagination.php";
+ ?>
     </div>
+</div>
 
 <?php
 require_once "transfer_add_modal.php";
 
 require_once "footer.php";
-
