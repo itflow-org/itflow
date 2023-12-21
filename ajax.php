d6660bff043125308fd6cf26516797f833bf35b3
johnnyq <johnny@pittpc.com>
2023-12-21 02:02:40 -0500

    Fix sending mail on item sharing

--- ajax.php
+++ ajax.php
@@ -310,7 +310,7 @@ if (isset($_GET['share_generate_link'])) {
         if ($item_expires_friendly == "never") {
             $subject = "$session_company_name secure link enclosed";
         }
-        $body = "Hello,<br><br>$session_name from $session_company_name sent you a time sensitive secure link regarding '$item_name'.<br><br>The link will expire in <strong>$item_expires_friendly</strong> and may only be viewed <strong>$item_view_limit</strong> times, before the link is destroyed. <br><br><strong><a href='$url'>Click here to access your secure content</a></strong><br><br>~<br>$session_company_name<br>Support Department<br>$config_ticket_from_email";
+        $body = mysqli_real_escape_string($mysqli, "Hello,<br><br>$session_name from $session_company_name sent you a time sensitive secure link regarding '$item_name'.<br><br>The link will expire in <strong>$item_expires_friendly</strong> and may only be viewed <strong>$item_view_limit</strong> times, before the link is destroyed. <br><br><strong><a href='$url'>Click here to access your secure content</a></strong><br><br>~<br>$session_company_name<br>Support Department<br>$config_ticket_from_email");
 
         $data = [
             [
@@ -319,7 +319,7 @@ if (isset($_GET['share_generate_link'])) {
                 'recipient' => $item_email,
                 'recipient_name' => $item_email,
                 'subject' => $subject,
-                'body' => $body,
+                'body' => $body
             ]
         ];
         
