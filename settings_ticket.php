db4fd095d6ef8e07529fa8ed24e85c6080d00369
johnnyq <johnny@pittpc.com>
2023-12-28 14:42:11 -0500

    Fix Undefined Checkbox POST vars in settings ticket

--- settings_ticket.php
+++ settings_ticket.php
@@ -9,6 +9,9 @@ require_once "inc_all_settings.php";
         <div class="card-body">
             <form action="post.php" method="post" autocomplete="off">
                 <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token'] ?>">
+                <input type="hidden" name="config_ticket_email_parse" value="0">
+                <input type="hidden" name="config_ticket_client_general_notifications" value="0">
+                <input type="hidden" name="config_ticket_autoclose" value="0">
 
                 <div class="form-group">
                     <label>Ticket Prefix</label>
