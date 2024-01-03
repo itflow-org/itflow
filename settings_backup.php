d2cd6f289f877d53c8917c8369c60a641830c344
AFTECH.RO <38830718+aftechro@users.noreply.github.com>
2024-01-03 22:55:46 +0000

    Update settings_backup.php

--- settings_backup.php
+++ settings_backup.php
@@ -77,6 +77,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['proceed-restore'])) {
                     $errorMessage = $conn->error;
                     $fullQuery = htmlspecialchars($query, ENT_QUOTES, 'UTF-8');
 
+                    // Display error message and stop execution
                     die("Error executing query: $errorMessage <br>Query: $fullQuery");
                 }
             }
@@ -166,7 +167,7 @@ function formatBytes($bytes, $decimals = 2)
     <div class="col-md-6">
         <div class="card card-dark mb-3">
             <div class="card-header py-3">
-                <h3 class="card-title"><i class="fas fa-fw fa-database mr-2"></i>Backup Database Maria 12</h3>
+                <h3 class="card-title"><i class="fas fa-fw fa-database mr-2"></i>Backup Database Maria 132</h3>
             </div>
             <div class="card-body" style="text-align: center;">
                 <form method="post">
