58abc29a6d463e622c79d0956981d72f9a7f39e5
AFTECH.RO <38830718+aftechro@users.noreply.github.com>
2024-01-04 10:08:04 +0000

    Update settings_backup.php

--- settings_backup.php
+++ settings_backup.php
@@ -179,7 +179,7 @@ function formatBytes($bytes, $decimals = 2)
     <div class="col-md-6">
         <div class="card card-dark mb-3">
             <div class="card-header py-3">
-                <h3 class="card-title"><i class="fas fa-fw fa-database mr-2"></i>Backup Database Maria 17</h3>
+                <h3 class="card-title"><i class="fas fa-fw fa-database mr-2"></i>Backup Database Maria 18</h3>
             </div>
             <div class="card-body" style="text-align: center;">
                 <form method="post">
