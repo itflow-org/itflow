87f1b9c0bee19d968d391e91e3cffc90ca75de4a
o-psi <andymalsb@gmail.com>
2024-01-10 22:16:40 +0000

    Add filter to Client Tickets

--- client_tickets.php
+++ client_tickets.php
@@ -18,10 +18,11 @@ if (isset($_GET['status']) && ($_GET['status']) == 'Open') {
     $ticket_status_snippet = "ticket_status != 'Closed'";
 }
 
-if (isset($_GET['unbilled']) && ($_GET['billable']) == '1') {
+if (isset($_GET['billable']) && ($_GET['billable']) == '1') {
     $billable = '1';
     $ticket_billable_snippet = " AND ticket_billable = 1";
-}
+    $ticket_status_snippet = $ticket_status . $ticket_billable_snippet;
+} 
 
 //Rebuild URL
 $url_query_strings_sort = http_build_query($get_copy);
