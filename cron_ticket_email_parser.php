d33d11acd2b3aa532b24463fd8a8ae2184c0ff0f
johnnyq <johnny@pittpc.com>
2024-01-11 00:34:36 -0500

    Set lock file cront ticket email parser once again from 300 sec aka 5 mins to 180 secs aka 3 mins

--- cron_ticket_email_parser.php
+++ cron_ticket_email_parser.php
@@ -62,7 +62,7 @@ $lock_file_path = "{$temp_dir}/itflow_email_parser_{$installation_id}.lock";
 if (file_exists($lock_file_path)) {
     $file_age = time() - filemtime($lock_file_path);
 
-    // If file is older than 5 minutes (300 seconds), delete and continue
+    // If file is older than 3 minutes (180 seconds), delete and continue
     if ($file_age > 300) {
         unlink($lock_file_path);
         mysqli_query($mysqli, "INSERT INTO logs SET log_type = 'Cron-Email-Parser', log_action = 'Delete', log_description = 'Cron Email Parser detected a lock file was present but was over 10 minutes old so it removed it'");
