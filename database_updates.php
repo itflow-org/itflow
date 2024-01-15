d35f1ff7fdd17b30c5a7e8e3a8c01f77ab8ef483
johnnyq <johnny@pittpc.com>
2024-01-15 13:08:46 -0500

    Increased the character limit for login uris, added login uri 2 and asset nat ip currently not exposed on the ui

--- database_updates.php
+++ database_updates.php
@@ -1549,14 +1549,24 @@ if (LATEST_DATABASE_VERSION > CURRENT_DATABASE_VERSION) {
         mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '1.0.1'");
     }
 
+    if (CURRENT_DATABASE_VERSION == '1.0.1') {
+        //Insert queries here required to update to DB version 1.0.2
+        mysqli_query($mysqli, "ALTER TABLE `logins` MODIFY `login_uri` VARCHAR(500) DEFAULT NULL");
+        mysqli_query($mysqli, "ALTER TABLE `logins` ADD `login_uri_2` VARCHAR(500) DEFAULT NULL AFTER `login_uri`");
+        mysqli_query($mysqli, "ALTER TABLE `assets` ADD `asset_nat_ip` VARCHAR(200) DEFAULT NULL AFTER `asset_ip`");
+
+        //Then, update the database to the next sequential version
+        mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '1.0.2'");
+    }
+
     // Be sure to change database_version.php to reflect the version you are updating to here
     // Please add this same comment block to the bottom of this file, and update the version number.
     // Uncomment Below Lines, to add additional database updates
     //
-    // if (CURRENT_DATABASE_VERSION == '1.0.1') {
-    //     // Insert queries here required to update to DB version 1.0.2
+    // if (CURRENT_DATABASE_VERSION == '1.0.2') {
+    //     // Insert queries here required to update to DB version 1.0.3
     //     // Then, update the database to the next sequential version
-    //     mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '1.0.2'");
+    //     mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '1.0.3'");
     // }
 
 } else {
