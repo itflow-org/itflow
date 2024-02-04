102481d09f1c0b0af90b291a8c1d3c4962660ba6
Marcus Hill <bcf8c9f2@opayq.com>
2024-02-04 14:51:58 +0000

    API - Add client create endpoint

--- api/v1/require_post_method.php
+++ api/v1/require_post_method.php
@@ -9,6 +9,6 @@ if ($_SERVER['REQUEST_METHOD'] !== "POST") {
 
 // Client ID must be specific for INSERT/UPDATE/DELETE queries
 // If this API key allows any client, set $client_id to the one specified, else leave it
-if ($client_id == 0) {
+if ($client_id == 0 && isset($_POST['client_id'])) {
     $client_id = intval($_POST['client_id']);
 }
