54e015b5a516e9ee1f6dfa6aa3cdd29e72637136
Marcus Hill <bcf8c9f2@opayq.com>
2024-02-24 11:56:18 +0000

    Add locations read api endpoint

---
+++ api/v1/locations/read.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../validate_api_key.php';
+
+require_once '../require_get_method.php';
+
+
+if (isset($_GET['location_id'])) {
+    // Location via ID (single)
+
+    $id = intval($_GET['location_id']);
+    $sql = mysqli_query($mysqli, "SELECT * FROM locations WHERE location_id = '$id' AND location_client_id LIKE '$client_id'");
+
+} else {
+    // All locations
+
+    $sql = mysqli_query($mysqli, "SELECT * FROM locations WHERE location_client_id LIKE '$client_id' ORDER BY location_id LIMIT $limit OFFSET $offset");
+}
+
+// Output
+require_once "../read_output.php";
+
