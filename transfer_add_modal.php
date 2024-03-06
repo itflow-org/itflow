a86523bd1b1f402fd9236489465fd0b9cdb54d0b
johnnyq <johnny@pittpc.com>
2024-03-05 19:26:16 -0500

    Removed the requirment visual for transer method as its not required

--- transfer_add_modal.php
+++ transfer_add_modal.php
@@ -144,7 +144,7 @@
                     </div>
 
                     <div class="form-group">
-                        <label>Transfer Method <strong class="text-danger">*</strong></label>
+                        <label>Transfer Method</label>
                         <div class="input-group">
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fa fa-fw fa-money-check-alt"></i></span>
