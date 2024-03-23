9e7f50b9eb4c8df72743762ce3926520bed215f3
johnnyq <johnny@pittpc.com>
2024-03-23 15:01:01 -0400

    Feature Add Bulk Cancel and Delete to Mail Queue for mail that are any status other than sent

--- post/setting.php
+++ post/setting.php
@@ -566,6 +566,63 @@ if (isset($_GET['cancel_mail'])) {
 
 }
 
+if (isset($_POST['bulk_cancel_emails'])) {
+    validateAdminRole();
+    validateCSRFToken($_POST['csrf_token']);
+
+    $count = 0; // Default 0
+    $email_ids = $_POST['email_ids']; // Get array of email IDs to be cancelled
+
+    if (!empty($email_ids)) {
+
+        // Cycle through array and mark each email as failed
+        foreach ($email_ids as $email_id) {
+
+            $email_id = intval($email_id);
+            mysqli_query($mysqli,"UPDATE email_queue SET email_status = 2, email_attempts = 99, email_failed_at = NOW() WHERE email_id = $email_id");
+
+            $count++;
+        }
+
+        // Logging
+        mysqli_query($mysqli, "INSERT INTO logs SET log_type = 'Email', log_action = 'Cancel', log_description = '$session_name bulk cancelled $count emails from the mail Queue', log_ip = '$session_ip', log_user_agent = '$session_user_agent', log_user_id = $session_user_id");
+
+        $_SESSION['alert_message'] = "Cancelled $count email(s)";
+
+    }
+
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
+}
+
+if (isset($_POST['bulk_delete_emails'])) {
+    validateAdminRole();
+    validateCSRFToken($_POST['csrf_token']);
+
+    $count = 0; // Default 0
+    $email_ids = $_POST['email_ids']; // Get array of email IDs to be deleted
+
+    if (!empty($email_ids)) {
+
+        // Cycle through array and delete each email
+        foreach ($email_ids as $email_id) {
+
+            $email_id = intval($email_id);
+            mysqli_query($mysqli,"DELETE FROM email_queue WHERE email_id = $email_id");
+
+            $count++;
+        }
+
+        // Logging
+        mysqli_query($mysqli, "INSERT INTO logs SET log_type = 'Email', log_action = 'Delete', log_description = '$session_name bulk deleted $count emails from the mail Queue', log_ip = '$session_ip', log_user_agent = '$session_user_agent', log_user_id = $session_user_id");
+
+        $_SESSION['alert_type'] = "danger";
+        $_SESSION['alert_message'] = "Deleted $count email(s)";
+
+    }
+
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
+}
+
 if (isset($_GET['download_database'])) {
 
     validateCSRFToken($_GET['csrf_token']);
