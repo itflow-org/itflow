6d8ab569c9d71a88ebf26f57688c89d24b6baf7d
johnnyq <johnny@pittpc.com>
2024-04-01 17:33:36 -0400

    Add Ticket subject to Add ticket to Project notification and logging

--- post/project.php
+++ post/project.php
@@ -80,13 +80,18 @@ if (isset($_POST['add_project_ticket'])) {
     $row = mysqli_fetch_array($sql);
     $client_id = intval($row['project_client_id']);
     $project_name = sanitizeInput($row['project_name']);
+
+    // Get Ticket Info
+    $sql = mysqli_query($mysqli, "SELECT * FROM tickets WHERE ticket_project_id = $project_id");
+    $row = mysqli_fetch_array($sql);
+    $ticket_subject = sanitizeInput($row['ticket_subject']);
     
     mysqli_query($mysqli, "UPDATE tickets SET ticket_project_id = $project_id WHERE ticket_id = $ticket_id");
 
     // Logging
-    mysqli_query($mysqli, "INSERT INTO logs SET log_type = 'Project', log_action = 'Edit', log_description = '$session_name added a ticket to project $project_name', log_ip = '$session_ip', log_user_agent = '$session_user_agent', log_client_id = $client_id, log_user_id = $session_user_id, log_entity_id = $project_id");
+    mysqli_query($mysqli, "INSERT INTO logs SET log_type = 'Project', log_action = 'Edit', log_description = '$session_name added a ticket $ticket_subject to project $project_name', log_ip = '$session_ip', log_user_agent = '$session_user_agent', log_client_id = $client_id, log_user_id = $session_user_id, log_entity_id = $project_id");
 
-    $_SESSION['alert_message'] = "You Added a Ticket to <strong>$project_name</strong>";
+    $_SESSION['alert_message'] = "You added Ticket <strong>$ticket_subject</strong> to <strong>$project_name</strong>";
 
     header("Location: " . $_SERVER["HTTP_REFERER"]);
 }
