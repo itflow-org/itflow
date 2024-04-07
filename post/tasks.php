dfb1304bfa3138cfc01814bbf383a34d52acf1bb
johnnyq <johnny@pittpc.com>
2024-04-07 14:26:37 -0400

    When completeing task add a private note to the ticket along with a 5 Min time worked soon will be configurable based off the task

--- post/tasks.php
+++ post/tasks.php
@@ -15,7 +15,6 @@ if (isset($_POST['add_task'])) {
     $sql = mysqli_query($mysqli, "SELECT * FROM tickets WHERE ticket_id = $ticket_id");
     $row = mysqli_fetch_array($sql);
     $client_id = intval($row['ticket_client_id']);
-
     
     mysqli_query($mysqli, "INSERT INTO tasks SET task_name = '$task_name', task_ticket_id = $ticket_id");
 
@@ -90,10 +89,15 @@ if (isset($_GET['complete_task'])) {
 
     mysqli_query($mysqli, "UPDATE tasks SET task_completed_at = NOW(), task_completed_by = $session_user_id WHERE task_id = $task_id");
 
+    // Add reply
+    mysqli_query($mysqli, "INSERT INTO ticket_replies SET ticket_reply = 'Completed Task - $task_name', ticket_reply_time_worked = '00:05:00', ticket_reply_type = 'Internal', ticket_reply_by = $session_user_id, ticket_reply_ticket_id = $ticket_id");
+
+    $ticket_reply_id = mysqli_insert_id($mysqli);
+
     // Logging
     mysqli_query($mysqli, "INSERT INTO logs SET log_type = 'Task', log_action = 'Edit', log_description = '$session_name completed task $task_name', log_ip = '$session_ip', log_user_agent = '$session_user_agent', log_client_id = $client_id, log_user_id = $session_user_id, log_entity_id = $task_id");
 
-    $_SESSION['alert_message'] = "You completed Task <strong>$task_name</strong>";
+    $_SESSION['alert_message'] = "You completed Task <strong>$task_name</strong> Great Job!<i class='far fa-4x fa-smile-wink ml-2'></i>";
 
     header("Location: " . $_SERVER["HTTP_REFERER"]);
 }
