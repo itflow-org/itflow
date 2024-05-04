17eb51bd54b3310de30557284bbacdb0de90d1d4
Hugo Sampaio <ssteeltm@gmail.com>
2024-05-04 19:23:39 -0300

    Update check_login.php If standard

--- check_login.php
+++ check_login.php
@@ -18,10 +18,11 @@ if (!isset($config_enable_setup) || $config_enable_setup == 1) {
 
 // Check user is logged in with a valid session
 if (!isset($_SESSION['logged']) || !$_SESSION['logged']) {
-    if($_SERVER["REQUEST_URI"] == "/")
+    if ($_SERVER["REQUEST_URI"] == "/") {
         header("Location: login.php");
-    else
+    } else {
         header("Location: login.php?last_visited=" . base64_encode($_SERVER["REQUEST_URI"]) );
+    }
     exit;
 }
 
