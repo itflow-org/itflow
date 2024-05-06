4779e8f923e56e0ad965f4cd8a367b4b6c193b05
Hugo Sampaio <ssteeltm@gmail.com>
2024-05-06 13:42:11 -0300

    add file ext .unf
    https://forum.itflow.org/d/835-allow-file-type
    
    added .unf for unifi backup file

--- client_file_upload_modal.php
+++ client_file_upload_modal.php
@@ -11,7 +11,7 @@
                 <input type="hidden" name="client_id" value="<?php echo $client_id; ?>">
                 <div class="modal-body bg-white">
 
-                    
+
                     <div class="form-group">
                         <label>Description</label>
                         <div class="input-group">
@@ -47,7 +47,7 @@
                     </div>
 
                     <div class="form-group">
-                        <input type="file" class="form-control-file" name="file[]" multiple id="fileInput" accept=".jpg, .jpeg, .gif, .png, .webp, .pdf, .txt, .md, .doc, .docx, .odt, .csv, .xls, .xlsx, .ods, .pptx, .odp, .zip, .tar, .gz, .xml, .msg, .json, .wav, .mp3, .ogg, .mov, .mp4, .av1, .ovpn, .cfg, .ps1, .vsdx, .drawio, .pfx">
+                        <input type="file" class="form-control-file" name="file[]" multiple id="fileInput" accept=".jpg, .jpeg, .gif, .png, .webp, .pdf, .txt, .md, .doc, .docx, .odt, .csv, .xls, .xlsx, .ods, .pptx, .odp, .zip, .tar, .gz, .xml, .msg, .json, .wav, .mp3, .ogg, .mov, .mp4, .av1, .ovpn, .cfg, .ps1, .vsdx, .drawio, .pfx, .unf">
                     </div>
                     <small class="text-secondary">Up to 20 files can be uploaded at once by holding down CTRL and selecting files</small>
 
