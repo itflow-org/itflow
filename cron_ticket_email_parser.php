95fd0344c041fcf94608ad97b607915f24588e1c
johnnyq <johnny@pittpc.com>
2024-06-27 17:23:45 -0400

    Fixed ticket Email processing showing array()

--- cron_ticket_email_parser.php
+++ cron_ticket_email_parser.php
@@ -80,7 +80,13 @@ function addTicket($contact_id, $contact_name, $contact_email, $client_id, $date
     $new_config_ticket_next_number = $ticket_number + 1;
     mysqli_query($mysqli, "UPDATE settings SET config_ticket_next_number = $new_config_ticket_next_number WHERE company_id = 1");
 
-    $message = "<i>Email from: $contact_email at $date:-</i> <br><br>$message";
+    // Clean up the message
+    $message = trim($message); // Remove leading/trailing whitespace
+    $message = preg_replace('/\s+/', ' ', $message); // Replace multiple spaces with a single space
+    $message = nl2br($message); // Convert newlines to <br>
+    
+    // Wrap the message in a div with controlled line height
+    $message = "<i>Email from: $contact_email at $date:-</i> <br><br><div style='line-height:1.5;'>$message</div>";
 
     $ticket_prefix_esc = mysqli_real_escape_string($mysqli, $config_ticket_prefix);
     $subject_esc = mysqli_real_escape_string($mysqli, $subject);
@@ -168,9 +174,15 @@ function addReply($from_email, $date, $subject, $ticket_number, $message, $attac
     global $mysqli, $config_app_name, $company_name, $company_phone, $config_ticket_prefix, $config_base_url, $config_ticket_from_name, $config_ticket_from_email, $config_smtp_host, $config_smtp_port, $config_smtp_encryption, $config_smtp_username, $config_smtp_password, $allowed_extensions;
 
     $ticket_reply_type = 'Client';
-    $message = explode("##- Please type your reply above this line -##", $message);
-    //$message = nl2br($message[0]);
-    $message = "<i>Email from: $from_email at $date:-</i> <br>$message";
+    // Clean up the message
+    $message_parts = explode("##- Please type your reply above this line -##", $message);
+    $message_body = $message_parts[0];
+    $message_body = trim($message_body); // Remove leading/trailing whitespace
+    $message_body = preg_replace('/\r\n|\r|\n/', ' ', $message_body); // Replace newlines with a space
+    $message_body = nl2br($message_body); // Convert remaining newlines to <br>
+
+    // Wrap the message in a div with controlled line height
+    $message = "<i>Email from: $from_email at $date:-</i> <br><br><div style='line-height:1.5;'>$message_body</div>";
 
     $ticket_number_esc = intval($ticket_number);
     $message_esc = mysqli_real_escape_string($mysqli, $message);
