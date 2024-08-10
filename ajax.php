693156c902ef891c1bb5adfc5762a839db99cb99
wrongecho <32306651+wrongecho@users.noreply.github.com>
2024-08-10 12:17:31 +0100

    Update ajax.php - Assets > Asset logging
    Adjust logging for asset changes so they are logged as type 'asset' not 'assets', to match existing logging

--- ajax.php
+++ ajax.php
@@ -176,7 +176,7 @@ if (isset($_POST['asset_set_notes'])) {
     mysqli_query($mysqli, "UPDATE assets SET asset_notes = '$notes' WHERE asset_id = $asset_id");
 
     // Logging
-    mysqli_query($mysqli, "INSERT INTO logs SET log_type = 'Assets', log_action = 'Modify', log_description = '$session_name modified asset notes', log_ip = '$session_ip', log_user_agent = '$session_user_agent', log_user_id = $session_user_id");
+    mysqli_query($mysqli, "INSERT INTO logs SET log_type = 'Asset', log_action = 'Modify', log_description = '$session_name modified asset notes', log_ip = '$session_ip', log_user_agent = '$session_user_agent', log_user_id = $session_user_id");
 
 }
 
