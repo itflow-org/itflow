f3e9c9c867672fbc9b01b7a134d4fffea36c66b2
wrongecho <marcus@itflow.org>
2024-09-11 18:02:18 +0100

    API Tidy
    - Remove old client ID queries, these are now built into the API keys. If a key has access to all clients, a client ID can be provided in the query.
    - Small fixes to assets URI lookup
    - Add read endpoint for vendors

--- api/v1/software/read.php
+++ api/v1/software/read.php
@@ -12,30 +12,21 @@ if (isset($_GET['software_id'])) {
 
 } elseif (isset($_GET['software_key'])) {
     // Specific software via key
-
     $key = mysqli_real_escape_string($mysqli, $_GET['software_license']);
     $sql = mysqli_query($mysqli, "SELECT * FROM software WHERE software_key = '$key' AND software_client_id LIKE '$client_id' ORDER BY software_id LIMIT $limit OFFSET $offset");
 
 } elseif (isset($_GET['software_name'])) {
     // Software by name
-
     $name = mysqli_real_escape_string($mysqli, $_GET['software_name']);
     $sql = mysqli_query($mysqli, "SELECT * FROM software WHERE software_name = '$name' AND software_client_id LIKE '$client_id' ORDER BY asset_id LIMIT $limit OFFSET $offset");
 
 } elseif (isset($_GET['software_type'])) {
     // Software via type
-
     $type = intval($_GET['software_type']);
     $sql = mysqli_query($mysqli, "SELECT * FROM software WHERE software_type = '$type' AND software_client_id LIKE '$client_id' ORDER BY software_id LIMIT $limit OFFSET $offset");
 
-} elseif (isset($_GET['client_id'])) {
-    // Software via client ID
-
-    $sql = mysqli_query($mysqli, "SELECT * FROM software WHERE software_client_id LIKE '$client_id' ORDER BY software_id LIMIT $limit OFFSET $offset");
-
 } else {
-    // All software(s)
-
+    // All software(s) (by client ID if given, or all in general if key permits)
     $sql = mysqli_query($mysqli, "SELECT * FROM software WHERE software_client_id LIKE '$client_id' ORDER BY software_id LIMIT $limit OFFSET $offset");
 }
 
