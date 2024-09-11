87af804cdb4e8a53a6c6bb1991a3aad659506220
wrongecho <marcus@itflow.org>
2024-09-11 18:02:18 +0100

    API Tidy
    - Remove old client ID queries, these are now built into the API keys. If a key has access to all clients, a client ID can be provided in the query.
    - Small fixes to assets URI lookup
    - Add read endpoint for vendors

--- api/v1/contacts/read.php
+++ api/v1/contacts/read.php
@@ -12,19 +12,16 @@ if (isset($_GET['contact_id'])) {
 
 } elseif (isset($_GET['contact_email'])) {
     // Specific contact via email (single)
-
     $email = mysqli_real_escape_string($mysqli, $_GET['contact_email']);
     $sql = mysqli_query($mysqli, "SELECT * FROM contacts WHERE contact_email = '$email' AND contact_client_id LIKE '$client_id'");
 
 } elseif (isset($_GET['contact_phone_or_mobile'])) {
     // Specific contact via phone number or mobile (single)
-
     $phone_or_mob = mysqli_real_escape_string($mysqli, $_GET['contact_phone_or_mobile']);
     $sql = mysqli_query($mysqli, "SELECT * FROM contacts WHERE contact_mobile = '$phone_or_mob' OR contact_phone = '$phone_or_mob' AND contact_client_id LIKE '$client_id' LIMIT 1");
 
 } else {
-    // All contacts
-
+    // All contacts (by client ID, or all in general if key permits)
     $sql = mysqli_query($mysqli, "SELECT * FROM contacts WHERE contact_client_id LIKE '$client_id' ORDER BY contact_id LIMIT $limit OFFSET $offset");
 }
 
--- api/v1/tickets/read.php
+++ api/v1/tickets/read.php
@@ -8,11 +8,15 @@ require_once '../require_get_method.php';
 // Specific ticket via ID (single)
 if (isset($_GET['ticket_id'])) {
     $id = intval($_GET['ticket_id']);
-    $sql = mysqli_query($mysqli, "SELECT * FROM tickets WHERE ticket_id = '$id' AND ticket_client_id LIKE '$client_id'");
+    $sql = mysqli_query(
+        $mysqli,
+        "SELECT * FROM tickets
+        LEFT JOIN ticket_statuses ON ticket_status = ticket_status_id
+        WHERE ticket_id = '$id' AND ticket_client_id LIKE '$client_id'"
+    );
 
 } else {
-    // All tickets
-
+    // All tickets (by client ID if given, or all in general if key permits)
     $sql = mysqli_query($mysqli, "SELECT * FROM tickets WHERE ticket_client_id LIKE '$client_id' ORDER BY ticket_id LIMIT $limit OFFSET $offset");
 }
 
