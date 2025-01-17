fd59709af53a59ea35fd4d53323a2fe940c55e2f
wrongecho <marcus@itflow.org>
2025-01-17 20:28:09 +0000

    Fix stripe auto-pay not fetching publishable key

--- js/autopay_setup_stripe.js
+++ js/autopay_setup_stripe.js
@@ -1,6 +1,4 @@
 // Initialize Stripe.js
-//const stripe = Stripe('pk_test_51QhHFaH1DlldW47DoEgLXkXD65rtGgpXrS2H9mlC9tF117NtExGVvdsVpnS0whz0s4wuTgN4Yl2hjW8ERXYHV3fP00ZKjrya8n');
-
 const stripe = Stripe(document.getElementById("stripe_publishable_key").value);
 
 initialize();
