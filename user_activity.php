8f0f585b59436e0cc6d9620b3db199fe674c0842
Fr3ak88 <42094080+Fr3ak88@users.noreply.github.com>
2025-01-30 14:50:49 +0100

    Update user_activity.php

--- user_activity.php
+++ user_activity.php
@@ -1,6 +1,13 @@
 <?php
-require_once "inc_all_user.php";
 
+// Sprachdatei laden
+include_once 'languages/lang.php';
+
+// Sprache setzen
+$selectedLang = $_SESSION['language'] ?? 'en'; // Standard: Englisch
+$langArray = loadLanguage($selectedLang);
+
+require_once "inc_all_user.php";
 
 $sql_recent_logins = mysqli_query($mysqli, "SELECT * FROM logs
     WHERE log_type = 'Login' OR log_type = 'Login 2FA' AND log_action = 'Success' AND log_user_id = $session_user_id
@@ -14,24 +21,21 @@ $sql_recent_logs = mysqli_query($mysqli, "SELECT * FROM logs
 
 ?>
 
+<!-- Recent Sign-ins Card -->
 <div class="card card-dark">
     <div class="card-header">
-        <h3 class="card-title"><i class="fas fa-fw fa-sign-in-alt mr-2"></i>Your Recent Sign ins</h3>
+        <h3 class="card-title"><i class="fas fa-fw fa-sign-in-alt mr-2"></i><?php echo lang('recent_sign_ins'); ?></h3>
     </div>
     <table class="table table-borderless">
         <tbody>
-        <?php
-
-        while ($row = mysqli_fetch_array($sql_recent_logins)) {
+        <?php while ($row = mysqli_fetch_array($sql_recent_logins)) { 
             $log_id = intval($row['log_id']);
             $log_ip = nullable_htmlentities($row['log_ip']);
             $log_user_agent = nullable_htmlentities($row['log_user_agent']);
             $log_user_os = getOS($log_user_agent);
             $log_user_browser = getWebBrowser($log_user_agent);
             $log_created_at = nullable_htmlentities($row['log_created_at']);
-
             ?>
-
             <tr>
                 <td><i class="fa fa-fw fa-clock text-secondary mr-2"></i><?php echo $log_created_at; ?></td>
                 <td><?php echo $log_user_os; ?></td>
@@ -42,27 +46,27 @@ $sql_recent_logs = mysqli_query($mysqli, "SELECT * FROM logs
         </tbody>
     </table>
     <div class="card-footer">
-        <a href="admin_audit_log.php?q=<?php echo "$session_name successfully logged in"; ?>>See More...</a>
+        <a href="admin_audit_log.php?q=<?php echo "$session_name successfully logged in"; ?>">
+            <?php echo lang('see_more'); ?>...
+        </a>
     </div>
 </div>
 
+<!-- Recent Activity Card -->
 <div class="card card-dark">
     <div class="card-header">
-        <h3 class="card-title"><i class="fas fa-fw fa-history mr-2"></i>Your Recent Activity</h3>
+        <h3 class="card-title"><i class="fas fa-fw fa-history mr-2"></i><?php echo lang('recent_activity'); ?></h3>
     </div>
-
-
     <table class="table">
         <tbody>
-        <?php
-
-        while ($row = mysqli_fetch_array($sql_recent_logs)) {
+        <?php while ($row = mysqli_fetch_array($sql_recent_logs)) { 
             $log_id = intval($row['log_id']);
             $log_type = nullable_htmlentities($row['log_type']);
             $log_action = nullable_htmlentities($row['log_action']);
             $log_description = nullable_htmlentities($row['log_description']);
             $log_created_at = nullable_htmlentities($row['log_created_at']);
-
+            
+            // Determine icon based on action
             if ($log_action == 'Create') {
                 $log_icon = "plus text-success";
             } elseif ($log_action == 'Modify') {
@@ -72,22 +76,19 @@ $sql_recent_logs = mysqli_query($mysqli, "SELECT * FROM logs
             } else {
                 $log_icon = "pencil";
             }
-
             ?>
-
             <tr>
                 <td><i class="fa fa-fw fa-clock text-secondary mr-2"></i><?php echo $log_created_at; ?></td>
                 <td><strong><i class="fa fa-fw text-secondary fa-<?php echo $log_icon; ?>"></i> <?php echo $log_type; ?></strong></td>
                 <td><span class="text-secondary"><?php echo $log_description; ?></span></td>
-
             </tr>
-            <?php
-        }
-        ?>
+        <?php } ?>
         </tbody>
     </table>
     <div class="card-footer">
-        <a href="admin_audit_log.php?q=<?php echo nullable_htmlentities($session_name); ?>">See More...</a>
+        <a href="admin_audit_log.php?q=<?php echo nullable_htmlentities($session_name); ?>">
+            <?php echo lang('see_more'); ?>...
+        </a>
     </div>
 </div>
 
