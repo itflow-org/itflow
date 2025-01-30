9cb1d93b8ef93bbd41d451f8f9a951807c37471e
Fr3ak88 <42094080+Fr3ak88@users.noreply.github.com>
2025-01-30 14:57:43 +0100

    Update user_details.php

--- user_details.php
+++ user_details.php
@@ -1,11 +1,18 @@
 <?php
-require_once "inc_all_user.php";
 
+// Sprachdatei laden
+include_once 'languages/lang.php';
+
+// Sprache setzen
+$selectedLang = $_SESSION['language'] ?? 'en'; // Standard: Englisch
+$langArray = loadLanguage($selectedLang);
+
+require_once "inc_all_user.php";
 ?>
 
 <div class="card card-dark">
     <div class="card-header py-3">
-        <h3 class="card-title"><i class="fas fa-fw fa-user mr-2"></i>Your User Details</h3>
+        <h3 class="card-title"><i class="fas fa-fw fa-user mr-2"></i><?php echo lang('your_user_details'); ?></h3>
     </div>
     <div class="card-body">
 
@@ -16,7 +23,7 @@ require_once "inc_all_user.php";
                 <?php if (empty($session_avatar)) { ?>
                     <i class="fas fa-user-circle fa-8x text-secondary"></i>
                 <?php } else { ?>
-                    <img alt="User avatar" src="<?php echo "uploads/users/$session_user_id/" . nullable_htmlentities($session_avatar); ?>" class="img-size-64">
+                    <img alt="<?php echo lang('user_avatar'); ?>" src="<?php echo "uploads/users/$session_user_id/" . nullable_htmlentities($session_avatar); ?>" class="img-size-64">
                 <?php } ?>
                 <h4 class="text-secondary mt-2"><?php echo nullable_htmlentities($session_user_role_display); ?></h4>
             </center>
@@ -24,41 +31,41 @@ require_once "inc_all_user.php";
             <hr>
 
             <div class="form-group">
-                <label>Your Name <strong class="text-danger">*</strong></label>
+                <label><?php echo lang('your_name'); ?> <strong class="text-danger">*</strong></label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fa fa-fw fa-user"></i></span>
                     </div>
-                    <input type="text" class="form-control" name="name" placeholder="Full Name" value="<?php echo stripslashes(nullable_htmlentities($session_name)); ?>" required>
+                    <input type="text" class="form-control" name="name" placeholder="<?php echo lang('full_name'); ?>" value="<?php echo stripslashes(nullable_htmlentities($session_name)); ?>" required>
                 </div>
             </div>
 
             <div class="form-group">
-                <label>Your Email <strong class="text-danger">*</strong></label>
+                <label><?php echo lang('your_email'); ?> <strong class="text-danger">*</strong></label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fa fa-fw fa-envelope"></i></span>
                     </div>
-                    <input type="email" class="form-control" name="email" placeholder="Email Address" value="<?php echo nullable_htmlentities($session_email); ?>" required>
+                    <input type="email" class="form-control" name="email" placeholder="<?php echo lang('email_address'); ?>" value="<?php echo nullable_htmlentities($session_email); ?>" required>
                 </div>
             </div>
 
             <div class="form-group">
-                <label>Your Avatar</label>
+                <label><?php echo lang('your_avatar'); ?></label>
                 <?php if ($session_avatar) { ?>
-                    <br><a href="post.php?clear_your_user_avatar&csrf_token=<?= $_SESSION['csrf_token'] ?>">Avatar is set, click to clear</a>
+                    <br><a href="post.php?clear_your_user_avatar&csrf_token=<?= $_SESSION['csrf_token'] ?>"><?php echo lang('avatar_clear_text'); ?></a>
                 <?php } else { ?>
                     <input type="file" class="form-control-file" accept="image/*" name="avatar">
                 <?php } ?>
             </div>
 
-            <button type="submit" name="edit_your_user_details" class="btn btn-primary btn-block mt-3"><i class="fas fa-check mr-2"></i>Save</button>
-
+            <button type="submit" name="edit_your_user_details" class="btn btn-primary btn-block mt-3">
+                <i class="fas fa-check mr-2"></i><?php echo lang('save'); ?>
+            </button>
 
         </form>
                 
     </div>
-
 </div>
 
 <?php
