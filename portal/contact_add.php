9159c8e2d37273ae6babbf5958a3abd03f30c570
Fr3ak88 <42094080+Fr3ak88@users.noreply.github.com>
2025-01-30 15:14:52 +0100

    Update contact_add.php

--- portal/contact_add.php
+++ portal/contact_add.php
@@ -1,12 +1,19 @@
 <?php
 /*
- * Client Portal
- * Contact management for PTC / technical contacts
- */
+* Client Portal
+* Contact management for PTC / technical contacts
+*/
 
 header("Content-Security-Policy: default-src 'self'");
 
 require_once "inc_portal.php";
+
+// Sprachdatei laden
+include_once 'languages/lang.php';
+
+// Sprache setzen
+$selectedLang = $_SESSION['language'] ?? 'en'; // Standard: Englisch
+$langArray = loadLanguage($selectedLang);
 
 if ($session_contact_primary == 0 && !$session_contact_is_technical_contact) {
     header("Location: portal_post.php?logout");
@@ -17,47 +24,46 @@ if ($session_contact_primary == 0 && !$session_contact_is_technical_contact) {
 
     <ol class="breadcrumb d-print-none">
         <li class="breadcrumb-item">
-            <a href="index.php">Home</a>
+            <a href="index.php"><?php echo lang('home'); ?></a>
         </li>
         <li class="breadcrumb-item">
-            <a href="contacts.php">Contacts</a>
+            <a href="contacts.php"><?php echo lang('contacts'); ?></a>
         </li>
-        <li class="breadcrumb-item active">Add Contact</li>
+        <li class="breadcrumb-item active"><?php echo lang('add_contact'); ?></li>
     </ol>
 
     <div class="col-md-6">
         <form action="portal_post.php" method="post">
-            <!-- Prevent undefined checkbox errors on submit -->
             <input type="hidden" name="contact_billing" value="0">
             <input type="hidden" name="contact_technical" value="0">
 
             <div class="form-group">
-                <label>Name <strong class="text-danger">*</strong></label>
+                <label><?php echo lang('name'); ?> <strong class="text-danger">*</strong></label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fa fa-fw fa-user"></i></span>
                     </div>
-                    <input type="text" class="form-control" name="contact_name" placeholder="Name" required maxlength="200">
+                    <input type="text" class="form-control" name="contact_name" placeholder="<?php echo lang('name'); ?>" required maxlength="200">
                 </div>
             </div>
 
             <div class="form-group">
-                <label>Email <strong class="text-danger">*</strong></label>
+                <label><?php echo lang('email'); ?> <strong class="text-danger">*</strong></label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fa fa-fw fa-envelope"></i></span>
                     </div>
-                    <input type="email" class="form-control" name="contact_email" placeholder="Email" required maxlength="200">
+                    <input type="email" class="form-control" name="contact_email" placeholder="<?php echo lang('email'); ?>" required maxlength="200">
                 </div>
             </div>
 
-            <label>Roles:</label>
+            <label><?php echo lang('roles'); ?>:</label>
             <div class="form-row">
                 <div class="col-md-4">
                     <div class="form-group">
                         <div class="custom-control custom-checkbox">
                             <input type="checkbox" class="custom-control-input" id="contactBillingCheckbox" name="contact_billing" value="1">
-                            <label class="custom-control-label" for="contactBillingCheckbox">Billing</label>
+                            <label class="custom-control-label" for="contactBillingCheckbox"><?php echo lang('billing'); ?></label>
                         </div>
                     </div>
                 </div>
@@ -65,32 +71,31 @@ if ($session_contact_primary == 0 && !$session_contact_is_technical_contact) {
                     <div class="form-group">
                         <div class="custom-control custom-checkbox">
                             <input type="checkbox" class="custom-control-input" id="contactTechnicalCheckbox" name="contact_technical" value="1">
-                            <label class="custom-control-label" for="contactTechnicalCheckbox">Technical</label>
+                            <label class="custom-control-label" for="contactTechnicalCheckbox"><?php echo lang('technical'); ?></label>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="form-group">
-                <label>Portal authentication</label>
+                <label><?php echo lang('portal_authentication'); ?></label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fa fa-fw fa-user-circle"></i></span>
                     </div>
                     <select class="form-control select2 authMethod" name="contact_auth_method">
-                        <option value="">- No portal access -</option>
-                        <option value="local">Local (Email and password)</option>
+                        <option value="">- <?php echo lang('no_portal_access'); ?> -</option>
+                        <option value="local"><?php echo lang('local_auth_method'); ?></option>
                         <?php if (!empty($config_azure_client_id)) { ?>
-                            <option value="azure">Azure (Microsoft 365)</option>
+                            <option value="azure"><?php echo lang('azure_auth_method'); ?></option>
                         <?php } ?>
                     </select>
                 </div>
             </div>
 
-            <button class="btn btn-primary" name="add_contact">Add</button>
+            <button class="btn btn-primary" name="add_contact"><?php echo lang('add'); ?></button>
         </form>
     </div>
-
 
 <?php
 require_once "portal_footer.php";
