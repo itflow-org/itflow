49dd75a6d652830fd74de9658faec66107fbcc60
Fr3ak88 <42094080+Fr3ak88@users.noreply.github.com>
2025-01-30 12:32:04 +0100

    Update trip_export_modal.php

--- trip_export_modal.php
+++ trip_export_modal.php
@@ -1,8 +1,19 @@
+<?php 
+
+// Sprachdatei laden
+include_once 'languages/lang.php';
+
+// Sprache setzen
+$selectedLang = $_SESSION['language'] ?? 'en'; // Standard: Englisch
+$langArray = loadLanguage($selectedLang);
+
+?>
+
 <div class="modal" id="exportTripsModal" tabindex="-1">
     <div class="modal-dialog">
         <div class="modal-content bg-dark">
             <div class="modal-header">
-                <h5 class="modal-title"><i class="fas fa-fw fa-download mr-2"></i>Export Trips to CSV</h5>
+                <h5 class="modal-title"><i class="fas fa-fw fa-download mr-2"></i><?php echo lang('export_trips_csv'); ?></h5>
                 <button type="button" class="close text-white" data-dismiss="modal">
                     <span>&times;</span>
                 </button>
@@ -11,7 +22,7 @@
                 <div class="modal-body bg-white">
 
                     <div class="form-group">
-                        <label>Date From</label>
+                        <label><?php echo lang('date_from'); ?></label>
                         <div class="input-group">
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fa fa-fw fa-calendar"></i></span>
@@ -21,7 +32,7 @@
                     </div>
 
                     <div class="form-group">
-                        <label>Date To</label>
+                        <label><?php echo lang('date_to'); ?></label>
                         <div class="input-group">
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fa fa-fw fa-calendar"></i></span>
@@ -32,8 +43,12 @@
 
                 </div>
                 <div class="modal-footer bg-white">
-                    <button type="submit" name="export_trips_csv" class="btn btn-primary text-bold"><i class="fas fa-fw fa-download mr-2"></i>Download CSV</button>
-                    <button type="button" class="btn btn-light" data-dismiss="modal"><i class="fas fa-times mr-2"></i>Cancel</button>
+                    <button type="submit" name="export_trips_csv" class="btn btn-primary text-bold">
+                        <i class="fas fa-fw fa-download mr-2"></i><?php echo lang('download_csv'); ?>
+                    </button>
+                    <button type="button" class="btn btn-light" data-dismiss="modal">
+                        <i class="fas fa-times mr-2"></i><?php echo lang('cancel'); ?>
+                    </button>
                 </div>
             </form>
         </div>
