985df6025a152f2d8b54ac82cd9e050717fd5bb1
Fr3ak88 <42094080+Fr3ak88@users.noreply.github.com>
2025-01-30 12:27:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <?php 
 
+// Sprachdatei laden
+include_once 'languages/lang.php';
+
+// Sprache setzen
+$selectedLang = $_SESSION['language'] ?? 'en'; // Standard: Englisch
+$langArray = loadLanguage($selectedLang);
+
 if (file_exists("config.php")) {
     include "inc_all.php";
  ?>
-	<!-- Breadcrumbs-->
-	<ol class="breadcrumb">
-	  <li class="breadcrumb-item">
-	    <a href="index.php">Dashboard</a>
-	  </li>
-	  <li class="breadcrumb-item active">Blank Page</li>
-	</ol>
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">
+        <a href="index.php"><?php echo lang('dashboard'); ?></a>
+      </li>
+      <li class="breadcrumb-item active"><?php echo lang('blank_page'); ?></li>
+    </ol>
 
-	<!-- Page Content -->
-	<h1>Blank Page</h1>
-	<hr>
-	<?php 
+    <!-- Page Content -->
+    <h1><?php echo lang('blank_page'); ?></h1>
+    <hr>
+    <?php 
 
-	include "footer.php";
-
+    include "footer.php";
 
 } else {
-	header("Location: setup.php");
+    header("Location: setup.php");
 }
 
 ?>
