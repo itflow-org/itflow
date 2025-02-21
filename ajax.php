073713a6a2dad5f91047faca5d6458e4d9e4692b
root <root@itflow.ng.local>
2025-02-21 13:30:26 -0300

    Drag and Drop Ticket Template Tasks

--- ajax.php
+++ ajax.php
@@ -706,3 +706,23 @@ if (isset($_POST['update_ticket_tasks_order'])) {
     echo json_encode(['status' => 'success']);
     exit;
 }
+
+if (isset($_POST['update_task_templates_order'])) {
+    // Update multiple task templates order
+    enforceUserPermission('module_support', 2);
+
+    $positions = $_POST['positions'];
+    $ticket_template_id = intval($_POST['ticket_template_id']);
+
+    foreach ($positions as $position) {   
+        $id = intval($position['id']);
+        $order = intval($position['order']);
+
+        mysqli_query($mysqli, "UPDATE task_templates SET task_template_order = $order WHERE task_template_ticket_template_id = $ticket_template_id AND task_template_id = $id");
+    }
+
+    // return a response
+    echo json_encode(['status' => 'success']);
+    exit;
+}
+
