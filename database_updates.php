ae59aa3326424d42187e98cf5da4c1aec280cb3d
Marcus Hill <bcf8c9f2@opayq.com>
2025-03-02 10:12:47 +0000

    Add SSL certificate history tracking

--- database_updates.php
+++ database_updates.php
@@ -2409,10 +2409,24 @@ if (LATEST_DATABASE_VERSION > CURRENT_DATABASE_VERSION) {
         mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '1.8.6'");
     }
 
-    // if (CURRENT_DATABASE_VERSION == '1.8.6') {
-    //     // Insert queries here required to update to DB version 1.8.7
+    if (CURRENT_DATABASE_VERSION == '1.8.6') {
+        mysqli_query($mysqli, "
+            CREATE TABLE `certificate_history` (`certificate_history_id` INT(11) NOT NULL AUTO_INCREMENT,
+            `certificate_history_column` VARCHAR(200) NOT NULL,
+            `certificate_history_old_value` TEXT NOT NULL,
+            `certificate_history_new_value` TEXT NOT NULL,
+            `certificate_history_certificate_id` INT(11) NOT NULL,
+            `certificate_history_modified_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`certificate_history_id`)) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
+        ");
+
+        mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '1.8.7'");
+    }
+
+    // if (CURRENT_DATABASE_VERSION == '1.8.8') {
+    //     // Insert queries here required to update to DB version 1.8.8
     //     // Then, update the database to the next sequential version
-    //     mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '1.8.7'");
+    //     mysqli_query($mysqli, "UPDATE `settings` SET `config_current_database_version` = '1.8.8'");
     // }
 
 } else {
