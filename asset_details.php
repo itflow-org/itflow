f53b77b55634e6e7c706e70a771f4d4d6c185c03
johnnyq <johnny@pittpc.com>
2025-05-21 11:51:18 -0400

    Migrated asset link models to the new ajax models this fixes the issue of the overlapping var asset_name and improves page load and performance in asset details

--- asset_details.php
+++ asset_details.php
@@ -378,27 +378,40 @@ if (isset($_GET['asset_id'])) {
                 <div class="dropdown dropleft">
                     <button type="button" class="btn btn-outline-primary" data-toggle="dropdown"><i class="fas fa-link mr-2"></i>Link</button>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item text-dark" href="#" data-toggle="modal" data-target="#linkSoftwareModal">
+                        <a class="dropdown-item text-dark" href="#"
+                            data-toggle="ajax-modal"
+                            data-ajax-url="ajax/ajax_asset_link_software.php"
+                            data-ajax-id="<?php echo $asset_id; ?>">
                             <i class="fa fa-fw fa-cube mr-2"></i>License
                         </a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item text-dark" href="#" data-toggle="modal" data-target="#linkCredentialModal">
+                        <a class="dropdown-item text-dark" href="#"
+                            data-toggle="ajax-modal"
+                            data-ajax-url="ajax/ajax_asset_link_credential.php"
+                            data-ajax-id="<?php echo $asset_id; ?>">
                             <i class="fa fa-fw fa-key mr-2"></i>Credential
                         </a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item text-dark" href="#" data-toggle="modal" data-target="#linkServiceModal">
+                        <a class="dropdown-item text-dark" href="#"
+                            data-toggle="ajax-modal"
+                            data-ajax-url="ajax/ajax_asset_link_service.php"
+                            data-ajax-id="<?php echo $asset_id; ?>">
                             <i class="fa fa-fw fa-stream mr-2"></i>Service
                         </a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item text-dark" href="#" data-toggle="modal" data-target="#linkDocumentModal">
+                        <a class="dropdown-item text-dark" href="#"
+                            data-toggle="ajax-modal"
+                            data-ajax-url="ajax/ajax_asset_link_document.php"
+                            data-ajax-id="<?php echo $asset_id; ?>">
                             <i class="fa fa-fw fa-folder mr-2"></i>Document
                         </a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item text-dark" href="#" data-toggle="modal" data-target="#linkFileModal">
+                        <a class="dropdown-item text-dark" href="#"
+                            data-toggle="ajax-modal"
+                            data-ajax-url="ajax/ajax_asset_link_file.php"
+                            data-ajax-id="<?php echo $asset_id; ?>">
                             <i class="fa fa-fw fa-paperclip mr-2"></i>File
-                        </a>
-                        
-                        
+                        </a>     
                     </div>
                 </div>
             </div>
@@ -1192,11 +1205,5 @@ require_once "modals/recurring_ticket_add_modal.php";
 require_once "modals/credential_add_modal.php";
 require_once "modals/client_document_add_modal.php";
 require_once "modals/client_file_upload_modal.php";
-
-require_once "modals/asset_link_software_modal.php";
-require_once "modals/asset_link_credential_modal.php";
-require_once "modals/asset_link_service_modal.php";
-require_once "modals/asset_link_document_modal.php";
-require_once "modals/asset_link_file_modal.php";
 
 require_once "includes/footer.php";
