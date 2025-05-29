0b66c8e1be4e248ebba670bb288b27997bf316b9
johnnyq <johnny@pittpc.com>
2025-05-29 13:29:27 -0400

    Better naming of Indetity Providers instead of Integrations to make way for more Indentity Provider like Google and custom SSO for the future

--- admin_identity_provider.php
+++ admin_identity_provider.php
@@ -4,13 +4,29 @@ require_once "includes/inc_all_admin.php";
 
 <div class="card card-dark">
     <div class="card-header py-3">
-        <h3 class="card-title"><i class="fas fa-fw fa-plug mr-2"></i>Integration Settings</h3>
+        <h3 class="card-title"><i class="fas fa-fw fa-fingerprint mr-2"></i>Identity Providers</h3>
     </div>
     <div class="card-body">
         <form action="post.php" method="post" autocomplete="off">
             <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token'] ?>">
 
             <h4>Client Portal SSO via Microsoft Entra</h4>
+
+            <div class="form-group">
+                <label>Identity Provider <small class='text-secondary'>(Currently only works with Microsft Entra)</small></label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fa fa-fw fa-fingerprint"></i></span>
+                    </div>
+                    <select class="form-control select2" readonly>
+                        <option <?php if (empty($config_azure_client_id)) { echo "selected"; } ?>>Disabled</option>
+                        <option <?php if ($config_azure_client_id) { echo "selected"; } ?>>Microsoft Entra</option>
+                        <option>Google (WIP)</option>
+                        <option>Custom SSO (WIP)</option>
+                    </select>
+                </div>
+            </div>
+
             <div class="form-group">
                 <label>MS Entra OAuth App (Client) ID</label>
                 <div class="input-group">
@@ -33,11 +49,10 @@ require_once "includes/inc_all_admin.php";
 
             <hr>
 
-            <button type="submit" name="edit_integrations_settings" class="btn btn-primary text-bold"><i class="fa fa-check mr-2"></i>Save</button>
+            <button type="submit" name="edit_identity_provider" class="btn btn-primary text-bold"><i class="fa fa-check mr-2"></i>Save</button>
 
         </form>
     </div>
 </div>
 
 <?php require_once "includes/footer.php";
-
