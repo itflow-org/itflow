181ea4b487935d9c64e70ba6f1c7980d3279a471
johnnyq <johnny@pittpc.com>
2025-09-25 13:43:47 -0400

    Remove dead router.php require

--- agent/includes/inc_client_overview_all.php
+++ agent/includes/inc_client_overview_all.php
@@ -2,7 +2,6 @@
 
 require_once "../config.php";
 require_once "../functions.php";
-require_once "../includes/router.php";
 require_once "../includes/check_login.php";
 require_once "../includes/page_title.php";
 require_once "../includes/header.php";
@@ -11,4 +10,3 @@ require_once "includes/client_overview_side_nav.php";
 require_once "../includes/inc_wrapper.php";
 require_once "../includes/inc_alert_feedback.php";
 require_once "../includes/filter_header.php";
-
