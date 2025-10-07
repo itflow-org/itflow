35a7506c26a930f9a378ad6b9250b77d94a367b8
johnnyq <johnny@pittpc.com>
2025-10-07 13:55:54 -0400

    Copy crons from /scripts to /cron, added custom directories for api/v1/, /setup, /cron and /scripts

---
+++ cron/custom/readme.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+
+- Custom Pages -
+     If you wish to add custom pages to ITFlow, add them to this directory"
+     Link to Documentation for File Directory Structure and examples
+*/
