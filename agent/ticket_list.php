ecce9949211bc0745593816b5a5c3606fbdc7be5
johnnyq <johnny@pittpc.com>
2025-10-16 11:43:26 -0400

    Used status var unstead of get status var for checkall

--- agent/ticket_list.php
+++ agent/ticket_list.php
@@ -9,7 +9,7 @@
                         <tr>
                             
                             <td>
-                                <?php if ($_GET['status'] !== 'Closed') { ?>
+                                <?php if ($status !== 'Closed') { ?>
                                 <div class="form-check">
                                     <input class="form-check-input" id="selectAllCheckbox" type="checkbox" onclick="checkAll(this)" onkeydown="checkAll(this)">
                                 </div>
