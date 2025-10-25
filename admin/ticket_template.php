0387e660669ea7611e6c1c540d6ee47393f789bd
johnnyq <johnny@pittpc.com>
2025-10-25 12:54:58 -0400

    Added Sortable Task Count in Ticket Templates Listing view

--- admin/ticket_template.php
+++ admin/ticket_template.php
@@ -8,10 +8,15 @@ require_once "includes/inc_all_admin.php";
 
 $sql = mysqli_query(
     $mysqli,
-    "SELECT SQL_CALC_FOUND_ROWS * FROM ticket_templates
-    WHERE (ticket_template_name LIKE '%$q%' OR ticket_template_description LIKE '%$q%')
-    AND ticket_template_archived_at IS NULL
-    ORDER BY $sort $order LIMIT $record_from, $record_to"
+    "SELECT SQL_CALC_FOUND_ROWS *, 
+            COUNT(task_template_id) AS task_count
+     FROM ticket_templates
+     LEFT JOIN task_templates ON task_template_ticket_template_id = ticket_template_id
+     WHERE (ticket_template_name LIKE '%$q%' OR ticket_template_description LIKE '%$q%')
+     AND ticket_template_archived_at IS NULL
+     GROUP BY ticket_template_id
+     ORDER BY $sort $order 
+     LIMIT $record_from, $record_to"
 );
 
 $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
@@ -46,14 +51,18 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
         <hr>
         <div class="table-responsive-sm">
             <table class="table table-striped table-borderless table-hover">
-                <thead class="text-dark <?php if($num_rows[0] == 0){ echo "d-none"; } ?>">
+                <thead class="text-dark <?php if($num_rows[0] == 0) { echo "d-none"; } ?>">
                 <tr>
                     <th>
-                        <a class="text-secondary" href="?<?php echo $url_query_strings_sort; ?>&sort=ticket_template_name&order=<?php echo $disp; ?>">
+                        <a class="text-secondary" href="?<?= $url_query_strings_sort ?>&sort=ticket_template_name&order=<?= $disp ?>">
                             Template <?php if ($sort == 'ticket_template_name') { echo $order_icon; } ?>
                         </a>
                     </th>
-                    <th>Tasks</th>
+                    <th>
+                        <a class="text-secondary" href="?<?php echo $url_query_strings_sort; ?>&sort=task_count&order=<?php echo $disp; ?>">
+                            Tasks <?php if ($sort == 'task_count') { echo $order_icon; } ?>
+                        </a>
+                    </th>
                     <th class="text-center">Action</th>
                 </tr>
                 </thead>
@@ -66,6 +75,7 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
                     $ticket_template_description = nullable_htmlentities($row['ticket_template_description']);
                     $ticket_template_subject = nullable_htmlentities($row['ticket_template_subject']);
                     $ticket_template_created_at = nullable_htmlentities($row['ticket_template_created_at']);
+                    $task_count = intval($row['task_count']);
 
                     ?>
                     <tr>
@@ -75,23 +85,23 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
                                     <i class="fa fa-fw fa-2x fa-life-ring mr-3"></i>
                                     <div class="media-body">
                                         <div>
-                                            <a href="ticket_template_details.php?ticket_template_id=<?php echo $ticket_template_id; ?>">
-                                                <?php echo $ticket_template_name; ?>
+                                            <a href="ticket_template_details.php?ticket_template_id=<?= $ticket_template_id ?>">
+                                                <?= $ticket_template_name ?>
                                             </a>
                                         </div>
-                                        <div><small class="text-secondary"><?php echo $ticket_template_description; ?></small></div>
+                                        <div><small class="text-secondary"><?= $ticket_template_description ?></small></div>
                                     </div>
                                 </div>
                             </a>
                         </td>
-                        <td>0</td>
+                        <td><?= $task_count ?></td>
                         <td>
                             <div class="dropdown dropleft text-center">
                                 <button class="btn btn-secondary btn-sm" data-toggle="dropdown">
                                     <i class="fas fa-ellipsis-h"></i>
                                 </button>
                                 <div class="dropdown-menu">
-                                    <a class="dropdown-item text-danger text-bold confirm-link" href="post.php?delete_ticket_template=<?php echo $ticket_template_id; ?>">
+                                    <a class="dropdown-item text-danger text-bold confirm-link" href="post.php?delete_ticket_template=<?= $ticket_template_id ?>">
                                         <i class="fas fa-fw fa-trash mr-2"></i>Delete
                                     </a>
                                 </div>
@@ -112,4 +122,3 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
 <?php
 require_once "modals/ticket_template/ticket_template_add.php";
 require_once "../includes/footer.php";
-
