691059aab21463971517e8a3ab59f545634b903c
wrongecho <marcus@itflow.org>
2026-08-13 09:45:01 +0100

    Debug - bump php memory flag to 512M

--- admin/debug.php
+++ admin/debug.php
@@ -114,12 +114,12 @@ $phpConfig[] = [
     'value' => $post_max_size,
 ];
 
-// PHP Memory Limit >= 128M
+// PHP Memory Limit >= 512M
 $memoryLimit = ini_get('memory_limit');
 $memoryLimitBytes = toBytes($memoryLimit);
-$memoryLimitPassed = $memoryLimitBytes >= (128 * 1024 * 1024);
+$memoryLimitPassed = $memoryLimitBytes >= (512 * 1024 * 1024);
 $phpConfig[] = [
-    'name' => 'PHP Memory Limit >= 128M',
+    'name' => 'PHP Memory Limit >= 512M',
     'passed' => $memoryLimitPassed,
     'value' => $memoryLimit,
 ];
