4f3d9f88b0709e778c605bc009bc4c1895867e79
wrongecho <32306651+wrongecho@users.noreply.github.com>
2026-09-06 20:33:39 +0100

    Fix typo in placeholder for abbreviation input
    (@BoredManCodes)

--- agent/modals/client/client_edit.php
+++ agent/modals/client/client_edit.php
@@ -101,7 +101,7 @@ ob_start();
                     <label>Shortened Name</label>
                     <div class="input-group">
                             <span class="input-group-text"><i class="fa fa-fw fa-id-badge"></i></span>
-                        <input type="text" class="form-control" name="abbreviation" placeholder="Shortned name for client - Max chars 6" value="<?= $client_abbreviation ?>" maxlength="6" oninput="this.value = this.value.toUpperCase()">
+                        <input type="text" class="form-control" name="abbreviation" placeholder="Shortened name for client - Max chars 6" value="<?= $client_abbreviation ?>" maxlength="6" oninput="this.value = this.value.toUpperCase()">
                     </div>
                 </div>
 
