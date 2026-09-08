cc909b67ae8011b52022fbeb1ef72c77ddbe44f6
Wrongecho <wrongecho@itflow.org>
2026-09-08 10:50:53 +0000

    API - Documents read should only show unarchived docs

--- api/v1/documents/read.php
+++ api/v1/documents/read.php
@@ -8,11 +8,11 @@ require_once '../require_get_method.php';
 if (isset($_GET['document_id'])) {
     // Document via ID (single)
     $id = intval($_GET['document_id']);
-    $sql = mysqli_query($mysqli, "SELECT * FROM documents WHERE document_id = '$id' AND 1=1 " . apiClientScopeSql('document_client_id') . "");
+    $sql = mysqli_query($mysqli, "SELECT * FROM documents WHERE document_id = '$id' AND document_archived_at IS NULL AND 1=1 " . apiClientScopeSql('document_client_id') . "");
 
 } else {
     // All documents (by client ID if given, or all in general if key permits)
-    $sql = mysqli_query($mysqli, "SELECT * FROM documents WHERE 1=1 " . apiClientScopeSql('document_client_id') . " ORDER BY document_id LIMIT $limit OFFSET $offset");
+    $sql = mysqli_query($mysqli, "SELECT * FROM documents WHERE document_archived_at IS NULL AND 1=1 " . apiClientScopeSql('document_client_id') . " ORDER BY document_id LIMIT $limit OFFSET $offset");
 }
 
 // Output
